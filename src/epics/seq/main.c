2d63ef82428a6d97f2a41536593fefc30f445e37
rolf <rolf@6dcd42c9-f523-4c6d-aada-af552506706e>
2005-10-31 14:37:13 +0000

    Generic Epics seq files.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@6 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/seq/main.c
@@ -0,0 +1,28 @@
+/* demoMain.c */
+/* Author:  Marty Kraimer Date:    17MAR2000 */
+
+/********************COPYRIGHT NOTIFICATION**********************************
+This software was developed under a United States Government license
+described on the COPYRIGHT_UniversityOfChicago file included as part
+of this distribution.
+****************************************************************************/
+
+/*
+ * Main program for demo sequencer
+ */
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "iocsh.h"
+
+int main(int argc,char *argv[])
+{
+    if(argc>=2) {
+        iocsh(argv[1]);
+	sleep(0xfffffff);
+    } else
+    	iocsh(NULL);
+    return(0);
+}
