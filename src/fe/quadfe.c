82f6740f214dee7b84c19c9ae03712e9b774d270
Rolf Bork <rolf.bork@ligo.org>
2005-11-10 01:52:48 +0000

    Added some diags.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@33 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/quadfe.c
+++ src/fe/quadfe.c
@@ -117,13 +117,6 @@ extern int myriNetReconnect(int waitReply, int dcuId);
 extern int myriNetCheckReconnect();
 
 
-
-/* Timing diag variables */
-int timeCycle;
-int timeHold = 0;
-int cycleHold = 0;
-int timeTest[10];
-
 /* ADC/DAC overflow variables */
 int overflowAdc[4][8];;
 int overflowDac[2][8];;
@@ -373,6 +366,7 @@ void *fe_start(void *arg)
   RFM_FE_COMMS *pEpicsComms;
   int cycleTime;
   int timeHold = 0;
+  int timeHoldMax = 0;
   int myGmError2 = 0;
   int attemptingReconnect = 0;
   int netRestored = 0;
@@ -400,6 +394,8 @@ void *fe_start(void *arg)
   double dWord[64];
   int dacOut[32];
   int dcuId;
+  static int adcTime;
+  static int adcHoldTime;
 
 
 
@@ -467,6 +463,7 @@ void *fe_start(void *arg)
 
   // Set an xtra TP to read out one pps signal
   testpoint[0] = (float *)&onePps;
+  pLocalEpics.epicsOutput.diagWord = 0;
 
 
   // Initialize DAQ function
@@ -484,9 +481,12 @@ void *fe_start(void *arg)
   printf("entering the loop\n");
   // Trigger the ADC to start running
   gsaAdcTrigger();
+  adcHoldTime = 0;
   /*  Run in continuous loop */
   while(!vmeDone){
 
+  	pLocalEpics.epicsOutput.diagWord = 0;
+        rdtscl(cpuClock[2]);
 	// Check ADC has data ready; hang here until it does
 	status = checkAdcRdy(ADC_SAMPLE_COUNT);
 	// Read CPU clock for timing info
@@ -547,214 +547,10 @@ void *fe_start(void *arg)
 	{
 		if(onePps < 1000) firstTime += 100;
 	}
+	if(onePps < 1000)  pLocalEpics.epicsOutput.onePps = clock16K;
+	if(pLocalEpics.epicsOutput.onePps > 4) pLocalEpics.epicsOutput.diagWord |= 1;
 
 	feCode(dWord,dacOut,dspPtr[0],dspCoeff,plocalEpics);
-
-#if 0
-
-	// Do M0 input filtering
-	for(ii=0;ii<6;ii++)
-	{
-		fmIn = dWord[ii];
-		m0SenOut[ii] = filterModuleD(&dsp,dspCoeff,ii,fmIn,0);
-	}
-
-	// Do M0 input matrix and DOF filtering
-	for(ii=0;ii<6;ii++)
-	{
-		kk = ii + 6;
-		fmIn =  m0SenOut[0] * pLocalEpics.epicsInput.m0InputMatrix[ii][0] +
-			m0SenOut[1] * pLocalEpics.epicsInput.m0InputMatrix[ii][1] +
-		 	m0SenOut[2] * pLocalEpics.epicsInput.m0InputMatrix[ii][2] +
-			m0SenOut[3] * pLocalEpics.epicsInput.m0InputMatrix[ii][3] +
-			m0SenOut[4] * pLocalEpics.epicsInput.m0InputMatrix[ii][4] +
-			m0SenOut[5] * pLocalEpics.epicsInput.m0InputMatrix[ii][5];
-		m0DofOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Do M0 output matrix and DOF filtering
-	for(ii=0;ii<6;ii++)
-	{
-		kk = ii + 12;
-		fmIn =  
-			m0DofOut[0] * pLocalEpics.epicsInput.m0OutputMatrix[ii][0] +
-			m0DofOut[1] * pLocalEpics.epicsInput.m0OutputMatrix[ii][1] +
-			m0DofOut[2] * pLocalEpics.epicsInput.m0OutputMatrix[ii][2] +
-			m0DofOut[3] * pLocalEpics.epicsInput.m0OutputMatrix[ii][3] +
-			m0DofOut[4] * pLocalEpics.epicsInput.m0OutputMatrix[ii][4] +
-			m0DofOut[5] * pLocalEpics.epicsInput.m0OutputMatrix[ii][5];
-		m0Out[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-		dacOut[ii] = (int)m0Out[ii];
-	}
-
-	// Do R0 input filtering
-	for(ii=0;ii<6;ii++)
-	{
-		kk = ii + 18;
-		fmIn = dWord[ii+6];
-		r0SenOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Do R0 input matrix and DOF filtering
-	for(ii=0;ii<6;ii++)
-	{
-		kk = ii + 24;
-		fmIn =  r0SenOut[0] * pLocalEpics.epicsInput.r0InputMatrix[ii][0] +
-			r0SenOut[1] * pLocalEpics.epicsInput.r0InputMatrix[ii][1] +
-		 	r0SenOut[2] * pLocalEpics.epicsInput.r0InputMatrix[ii][2] +
-			r0SenOut[3] * pLocalEpics.epicsInput.r0InputMatrix[ii][3] +
-			r0SenOut[4] * pLocalEpics.epicsInput.r0InputMatrix[ii][4] +
-			r0SenOut[5] * pLocalEpics.epicsInput.r0InputMatrix[ii][5];
-		r0DofOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Do R0 output matrix and DOF filtering
-	for(ii=0;ii<6;ii++)
-	{
-		kk = ii + 30;
-		fmIn =  
-			r0DofOut[0] * pLocalEpics.epicsInput.r0OutputMatrix[ii][0] +
-			r0DofOut[1] * pLocalEpics.epicsInput.r0OutputMatrix[ii][1] +
-			r0DofOut[2] * pLocalEpics.epicsInput.r0OutputMatrix[ii][2] +
-			r0DofOut[3] * pLocalEpics.epicsInput.r0OutputMatrix[ii][3] +
-			r0DofOut[4] * pLocalEpics.epicsInput.r0OutputMatrix[ii][4] +
-			r0DofOut[5] * pLocalEpics.epicsInput.r0OutputMatrix[ii][5];
-		r0Out[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-		dacOut[ii+6] = (int)r0Out[ii];
-	}
-
-	// Do L1 input filtering
-	for(ii=0;ii<4;ii++)
-	{
-		kk = ii + FILT_L1_ULSEN;
-		fmIn = dWord[ii+12];
-		l1SenOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Do L1 input matrix and DOF filtering
-	for(ii=0;ii<3;ii++)
-	{
-		kk = ii + FILT_L1_POS;
-		fmIn =  l1SenOut[0] * pLocalEpics.epicsInput.l1InputMatrix[0][ii] +
-			l1SenOut[1] * pLocalEpics.epicsInput.l1InputMatrix[1][ii] +
-		 	l1SenOut[2] * pLocalEpics.epicsInput.l1InputMatrix[2][ii] +
-		 	l1SenOut[3] * pLocalEpics.epicsInput.l1InputMatrix[3][ii];
-		l1DofOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Place for LSC, ASCP, ASCY
-	for(ii=0;ii<3;ii++)
-	{
-		kk = ii + FILT_L1_LSC;
-		fmIn = dWord[ii+12];
-		l1DofOut[ii] += filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// L1 Output Filter Matrix
-	l1Out[0] =
-		filterModuleD(&dsp,dspCoeff,FILT_L1_ULPOS,l1DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_ULPIT,l1DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_ULYAW,l1DofOut[2],0);
-
-	l1Out[1] =
-		filterModuleD(&dsp,dspCoeff,FILT_L1_LLPOS,l1DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_LLPIT,l1DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_LLYAW,l1DofOut[2],0);
-
-	l1Out[2] =
-		filterModuleD(&dsp,dspCoeff,FILT_L1_URPOS,l1DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_URPIT,l1DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_URYAW,l1DofOut[2],0);
-
-	l1Out[3] =
-		filterModuleD(&dsp,dspCoeff,FILT_L1_LRPOS,l1DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_LRPIT,l1DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L1_LRYAW,l1DofOut[2],0);
-
-	// L1 Output Filters
-	for(ii=0;ii<4;ii++)
-	{
-		kk = ii + FILT_L1_ULOUT;
-		fmIn = l1Out[ii];
-		l1Out[ii] += filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-		dacOut[ii+12] = (int)l1Out[ii];
-	}
-
-	// Do L2 input filtering
-	for(ii=0;ii<4;ii++)
-	{
-		kk = ii + FILT_L2_ULSEN;
-		fmIn = dWord[ii+16];
-		l2SenOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Do L2 input matrix and DOF filtering
-	for(ii=0;ii<3;ii++)
-	{
-		kk = ii + FILT_L2_POS;
-		fmIn =  l2SenOut[0] * pLocalEpics.epicsInput.l2InputMatrix[0][ii] +
-			l2SenOut[1] * pLocalEpics.epicsInput.l2InputMatrix[1][ii] +
-		 	l2SenOut[2] * pLocalEpics.epicsInput.l2InputMatrix[2][ii] +
-		 	l2SenOut[3] * pLocalEpics.epicsInput.l2InputMatrix[3][ii];
-		l2DofOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Place for LSC, ASCP, ASCY
-	for(ii=0;ii<3;ii++)
-	{
-		kk = ii + FILT_L2_LSC;
-		fmIn = dWord[ii+12];
-		l2DofOut[ii] += filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// L2 Output Filter Matrix
-	l2Out[0] =
-		filterModuleD(&dsp,dspCoeff,FILT_L2_ULPOS,l2DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_ULPIT,l2DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_ULYAW,l2DofOut[2],0);
-
-	l2Out[1] =
-		filterModuleD(&dsp,dspCoeff,FILT_L2_LLPOS,l2DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_LLPIT,l2DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_LLYAW,l2DofOut[2],0);
-
-	l2Out[2] =
-		filterModuleD(&dsp,dspCoeff,FILT_L2_URPOS,l2DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_URPIT,l2DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_URYAW,l2DofOut[2],0);
-
-	l2Out[3] =
-		filterModuleD(&dsp,dspCoeff,FILT_L2_LRPOS,l2DofOut[0],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_LRPIT,l2DofOut[1],0) +
-		filterModuleD(&dsp,dspCoeff,FILT_L2_LRYAW,l2DofOut[2],0);
-
-	// L2 Output Filters
-	for(ii=0;ii<4;ii++)
-	{
-		kk = ii + FILT_L2_ULOUT;
-		fmIn = l2Out[ii];
-		l2Out[ii] += filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Place for L3 LSC, ASCP, ASCY
-	for(ii=0;ii<3;ii++)
-	{
-		kk = ii + FILT_L3_LSC;
-		fmIn = dWord[ii+12];
-		l3DofOut[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-
-	// Do R0 output matrix and DOF filtering
-	for(ii=0;ii<5;ii++)
-	{
-		kk = ii + FILT_L3_ULOUT;
-		fmIn =  
-			l3DofOut[0] * pLocalEpics.epicsInput.l3OutputMatrix[0][ii] +
-			l3DofOut[1] * pLocalEpics.epicsInput.l3OutputMatrix[1][ii] +
-			l3DofOut[2] * pLocalEpics.epicsInput.l3OutputMatrix[2][ii];
-		l3Out[ii] = filterModuleD(&dsp,dspCoeff,kk,fmIn,0);
-	}
-#endif
 
 	// Check Dac output overflow and write to DMA buffer
 	pDacData = (unsigned int *)_pci_dac;
@@ -784,19 +580,18 @@ void *fe_start(void *arg)
 				// Set error flag, which will cause daqLib to quit sending
 				// data to FB until problem is fixed.
 				myGmError2 = 1;
-				if(!attemptingReconnect)
-				{
-					attemptingReconnect = 2;
-					// Send a reconnect request to FB.
-					status = myriNetReconnect(0,dcuId);
-					netRestored = 0;
-					printf("Net fail - recon try\n");
-				}
+				attemptingReconnect = 2;
+				// Send a reconnect request to FB.
+				status = myriNetReconnect(0,dcuId);
+				netRestored = 0;
+				printf("Net fail - recon try\n");
+				pLocalEpics.epicsOutput.diagWord |= 4;
 			}
 		}
 		// If net reconnect requested, check for receipt of return message from FB
 		if(attemptingReconnect == 2) 
 		{
+			pLocalEpics.epicsOutput.diagWord |= 4;
 			status = myriNetCheckReconnect();
 			if(status == 0)
 			{
@@ -834,12 +629,26 @@ void *fe_start(void *arg)
 
 	cycleTime = (cpuClock[1] - cpuClock[0])/CPURATE;
 	if(cycleTime > timeHold) timeHold = cycleTime;
+	if(cycleTime > timeHoldMax) timeHoldMax = cycleTime;
+	adcTime = (cpuClock[0] - cpuClock[2])/CPURATE;
+	if(adcTime > adcHoldTime) adcHoldTime = adcTime;
 
         if((subcycle == 0) && (daqCycle == 15))
         {
 	  pLocalEpics.epicsOutput.cpuMeter = timeHold;
+	  pLocalEpics.epicsOutput.cpuMeterMax = timeHoldMax;
           timeHold = 0;
-	        if(myGmError2 != 0) printf("Network error %d %d %d\n",myGmError2,attemptingReconnect,netRestored);
+	  pLocalEpics.epicsOutput.adcWaitTime = adcHoldTime;
+	  if(adcHoldTime > 60) pLocalEpics.epicsOutput.diagWord |= 2;
+	  if(timeHoldMax > 60) pLocalEpics.epicsOutput.diagWord |= 8;
+  	  if(pLocalEpicsRfm->epicsInput.diagReset)
+	  {
+		pLocalEpicsRfm->epicsInput.diagReset = 0;
+		adcHoldTime = 0;
+		timeHoldMax = 0;
+printf("DIAG RESET\n");
+	  }
+	  if(myGmError2 != 0) printf("Network error %d %d %d\n",myGmError2,attemptingReconnect,netRestored);
         }
 
   }
