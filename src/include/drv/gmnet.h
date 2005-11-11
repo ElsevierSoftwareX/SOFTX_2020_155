5e4034aa40392e220e7852ea2d6a40137fdf393e
rolf <rolf@6dcd42c9-f523-4c6d-aada-af552506706e>
2005-11-11 01:26:56 +0000

    Removed variable declarations.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@35 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/drv/gmnet.h
+++ src/include/drv/gmnet.h
@@ -7,7 +7,7 @@
 
 #define GM_DAQ_PRIORITY GM_LOW_PRIORITY
 
-#define GM_RCV_MESSAGE_SIZE	9 
+#define GM_RCV_MESSAGE_SIZE	10 
 #define GM_16HZ_SIZE	21 
 #define GM_16HZ_SEND_SIZE	17 
 #define GM_RCV_BUFFER_COUNT 1
@@ -27,23 +27,6 @@ typedef struct				/* Receiver-to-sender ID message */
   gm_u32_n_t slack;			/* Make length a multiple of 64 */
 } gm_s_e_id_message_t;
 
-static void *netOutBuffer;
-static void *netInBuffer;
-static void *netDmaBuffer;
-gm_remote_ptr_t directed_send_addr;
-gm_remote_ptr_t directed_send_subaddr[16];
-gm_recv_event_t *event;
-
-int my_board_num = 0;                 /* Default board_num is 0 */
-gm_u32_t receiver_node_id;
-gm_u32_t receiver_global_id;
-gm_status_t main_status;
-void *recv_buffer;
-unsigned int size;
-gm_s_e_id_message_t *id_message;
-gm_u32_t recv_length;
-static int expected_callbacks = 0;
-static struct gm_port *netPort = 0;
 
 #define GM_DAQ_XFER_SIZE       1024
 #define GM_DAQ_XFER_BYTE       GM_DAQ_XFER_SIZE * 4
@@ -61,7 +44,7 @@ typedef struct
   unsigned int blockCrc;
   unsigned int dataCount;
   int tpCount;
-  int tpNum[32];
+  int tpNum[20];
 }daqMessage;
 
 /* Definition of data sent to Framebuilder */
@@ -69,7 +52,3 @@ typedef struct
 {
   unsigned int data[GM_DAQ_XFER_SIZE];
 }daqData;
-
-daqMessage *daqSendMessage;
-daqData *daqSendData;
-
