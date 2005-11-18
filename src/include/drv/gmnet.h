989d881c03d46d0c7b6b402a5f531fcae08ca7e0
rolf <rolf@6dcd42c9-f523-4c6d-aada-af552506706e>
2005-11-18 21:02:43 +0000

    Increased defs to allow 2MByte/sec DAQ buffers.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@67 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/drv/gmnet.h
+++ src/include/drv/gmnet.h
@@ -8,7 +8,7 @@
 #define GM_DAQ_PRIORITY GM_LOW_PRIORITY
 
 #define GM_RCV_MESSAGE_SIZE	 9
-#define GM_16HZ_SIZE	21 
+#define GM_16HZ_SIZE	22 
 #define GM_16HZ_SEND_SIZE	17 
 #define GM_RCV_BUFFER_COUNT 1
 #define GM_16HZ_BUFFER_COUNT 1
@@ -27,8 +27,9 @@ typedef struct				/* Receiver-to-sender ID message */
   gm_u32_n_t slack;			/* Make length a multiple of 64 */
 } gm_s_e_id_message_t;
 
-#define GM_DAQ_XFER_SIZE       1024
-#define GM_DAQ_XFER_BYTE       GM_DAQ_XFER_SIZE * 4
+#define GM_DAQ_XFER_SIZE        2048
+#define GM_DAQ_BLOCK_SIZE	GM_DAQ_XFER_SIZE * 16
+#define GM_DAQ_XFER_BYTE        GM_DAQ_XFER_SIZE * 4
 
 /* Definition of message struct sent to Framebuilder each 16Hz cycle */
 typedef struct
