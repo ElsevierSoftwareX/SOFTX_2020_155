a4103e8d3799b500c6b6342da9b6c3c0480fefd1
Rolf Bork <rolf.bork@ligo.org>
2005-12-21 21:42:25 +0000

    Epics headers.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@107 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/include/param.h
@@ -0,0 +1,22 @@
+#ifndef PARAM_H
+#define PARAM_H
+
+typedef struct CHAN_PARAM {
+  int dcuid;
+  int datarate;
+  int acquire;
+  int ifoid;
+  int datatype;
+  int chnnum;
+  int testpoint;
+  float gain;
+  float slope;
+  float offset;
+  char units[40];
+} CHAN_PARAM;
+
+int parseConfigFile(char *, unsigned long *, int (*callback)(char *, struct CHAN_PARAM *, void *), int, char *, void *);
+
+int loadDaqConfigFile(DAQ_INFO_BLOCK *, char *, char *, char *);
+
+#endif
