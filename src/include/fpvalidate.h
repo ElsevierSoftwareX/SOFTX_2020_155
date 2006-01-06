821a5a03b0c2d4f72392149b9c2cd2238f03a960
rolf <rolf@6dcd42c9-f523-4c6d-aada-af552506706e>
2006-01-06 20:51:04 +0000

    Floating point validation software.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@116 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/include/fpvalidate.h
@@ -0,0 +1,11 @@
+INLINE float 
+fpvalidate(float p) {
+ float f = p;
+
+ if (((*((unsigned long *)&f)) & 0x7f800000) == 0x7f800000)
+ 	return 0.000000000001;
+ if(f > 1e20) f = 1e20;
+ if(f < -1e20) f = -1e20;
+  
+ return f;
+}
