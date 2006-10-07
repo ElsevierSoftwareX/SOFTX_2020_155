53e0c63a435a21db5f9b58c31b4eac83a6182080
Alexander Ivanov <alexander.ivanov@ligo.org>
2006-10-07 00:57:09 +0000

    Added new parts to the library.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@349 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Product.pm
@@ -0,0 +1,80 @@
+package CDS::Product;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Product;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+	print ::OUTH "\tfloat $::xpartName[$i];\n";
+        print ::OUTH "\tint $::xpartName[$i]\_TRAMP;\n";
+        print ::OUTH "\tint $::xpartName[$i]\_RMON;\n";
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+	print ::EPICS "INVARIABLE $::xpartName[$i] $::systemName\.$::xpartName[$i] float ai 0 field(PREC,\"3\")\n";
+        print ::EPICS "INVARIABLE $::xpartName[$i]\_TRAMP $::systemName\.$::xpartName[$i]\_TRAMP int ai 0 field(PREC,\"0\")\n";
+        print ::EPICS "OUTVARIABLE $::xpartName[$i]\_RMON $::systemName\.$::xpartName[$i]\_RMON int ai 0 field(PREC,\"0\")\n";
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "double \L$::xpartName[$i]\[$::partOutCnt[$i]\];\n";
+        print ::OUT "float $::xpartName[$i]\_CALC;\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        my $fromPort = $::partInputPort[$i][$j];
+        return "\L$::xpartName[$from]" . "\[" . $fromPort . "\]";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+	return "";
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+        my $calcExp = "// PRODUCT\n";
+        $calcExp .= "pLocalEpics->$::systemName\.$::xpartName[$i]";
+        $calcExp .= "_RMON = \n\tgainRamp(pLocalEpics->$::systemName\.$::xpartName[$i],";
+        $calcExp .= "pLocalEpics->$::systemName\.$::xpartName[$i]";
+        $calcExp .= "_TRAMP,";
+        $calcExp .= "$::gainCnt\,\&$::xpartName[$i]\_CALC);";
+        $calcExp .= "\n\n";
+        for (0 .. $inCnt-1) {
+          $calcExp .= "\L$::xpartName[$i]";
+          $calcExp .= "\[";
+          $calcExp .= $_;
+          $calcExp .= "\] = ";
+          $calcExp .= "$::xpartName[$i]";
+          $calcExp .= "_CALC * ";
+          $calcExp .= $::fromExp[$_];
+          $calcExp .= ";\n";
+        }
+        $gainCnt ++;
+	return $calcExp;
+}
---
+++ src/epics/util/lib/Rms.pm
@@ -0,0 +1,65 @@
+package CDS::Rms;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Rms;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "float \L$::xpartName[$i];\n";
+        print ::OUT "static float \L$::xpartName[$i]\_avg;\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        return "";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+	return "\L$::xpartName[$i]\_avg = 0\.0;\n";
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+        my $calcExp = "// RMS\n";
+        $calcExp .= "\L$::xpartName[$i]";
+        $calcExp .= " = ";
+        $calcExp .= $::fromExp[0];
+        $calcExp .= ";\n";
+        $calcExp .= "if(\L$::xpartName[$i]\ \> 2000\) \L$::xpartName[$i] = 2000;\n";
+        $calcExp .= "if(\L$::xpartName[$i]\ \< -2000\) \L$::xpartName[$i] = 2000;\n";
+        $calcExp .= "\L$::xpartName[$i] = \L$::xpartName[$i] * \L$::xpartName[$i];\n";
+        $calcExp .= "\L$::xpartName[$i]\_avg = \L$::xpartName[$i] * \.00005 + ";
+        $calcExp .= "\L$::xpartName[$i]\_avg * 0\.99995;\n";
+        $calcExp .= "\L$::xpartName[$i] = lsqrt(\L$::xpartName[$i]\_avg);\n";
+	return $calcExp;
+}
