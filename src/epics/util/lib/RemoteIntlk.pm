c9390fecd1690a25ff62711c3f00c31bbf35584f
Rolf Bork <rolf.bork@ligo.org>
2006-10-23 22:05:05 +0000

    Matlab part to send epics interlock signal from one system to another.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@357 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/RemoteIntlk.pm
@@ -0,0 +1,66 @@
+package CDS::RemoteIntlk;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return RemoteIntlk;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+	$temp = $::xpartName[$i];
+	$temp =~ s/\-/\_/g;
+        print ::OUTH "\tint $temp;\n";
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+	$temp = $::xpartName[$i];
+	$temp =~ s/\-/\_/g;
+        print ::EPICS "REMOTE_INTLK $::xpartName[$i] $::systemName\.$temp int ai 0 field(PREC,\"3\")\n";
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+        my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        return "pLocalEpics->" . $::systemName . "\." . $::xpartName[$from];
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+        print "Found EPICS REMOTE INTLK $::xpartName[$i] $::partInputType[$i][0] in loop\n";
+	$temp = $::xpartName[$i];
+	$temp =~ s/\-/\_/g;
+        my $ret = "// RemoteIntlk\n";
+        $ret .= "pLocalEpics->$::systemName\.$temp = ";
+        $ret .= "$::fromExp[0];\n";
+	return $ret;
+}
