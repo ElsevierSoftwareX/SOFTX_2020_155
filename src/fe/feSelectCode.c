d8a6e7a87f1c277e3d457bf270c47f45d6dc211a
Alexander Ivanov <alexander.ivanov@ligo.org>
2006-10-27 21:47:21 +0000

    Fixed bad SEI names
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@414 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/feSelectCode.c
+++ src/fe/feSelectCode.c
@@ -1,6 +1,6 @@
 #ifdef SEI_CODE
 	#include "drv/seiwd.c"	/* User code for HEPI control.		*/
-	#include "hepi/hepi.c"	/* User code for HEPI control.		*/
+	#include "sei/sei.c"	/* User code for HEPI control.		*/
 #elif defined(SUS_CODE)
 	#include "sus/sus.c"	/* User code for quad control.		*/
 #elif defined(PNM)
@@ -12,8 +12,13 @@
 	volatile float *lscRfmPtr = 0;
 #elif defined(LTB_CODE)
 	#include "ltb/ltb.c"	/* User code for Ponderomotive control. */
+<<<<<<< feSelectCode.c
+#elif defined(SEI_CODE)
+	#include "sei/sei.c"
+=======
 #elif defined(DBB_CODE)
 	#include "dbb/dbb.c"
+>>>>>>> 1.2
 #else
 	#error
 #endif
