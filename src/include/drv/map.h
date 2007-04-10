79f9e5e4e4b703e2a34383cc5d9a8cfc66a92509
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2007-04-10 16:59:39 +0000

    *** empty log message ***
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@619 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/include/drv/map.h
@@ -0,0 +1,17 @@
+#ifndef MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
+int mapPciModules(CDS_HARDWARE *);/* Init routine to map adc/dac cards*/
+int gsaAdcTrigger(int,int[]);	/* Starts ADC acquisition.	*/
+int gsaAdcStop(void);		/* Stop acquistion		*/
+int adcDmaDone(int,int *);	/* Checks if ADC DMA complete.	*/
+int dacDmaDone(int);
+int checkAdcRdy(int,int);	/* Checks if ADC has samples avail */
+int gsaAdcDma1(int,int,int);	/* Setup ADC DMA registers	*/
+void gsaAdcDma2(int);		/* Send GO bit to ADC DMA registers*/
+int gsaDacDma1(int,int);		/* Setup DAC DMA registers.	*/
+void gsaDacDma2(int,int);	/* Send GO bit to DAC DMA registers.*/
+unsigned int readDio(CDS_HARDWARE *,int);
+/*int mapRfm(CDS_HARDWARE *pHardware, struct pci_dev *rfmdev);*/
+void writeDio(CDS_HARDWARE *pHardware, int modNum, int data);
+#endif
