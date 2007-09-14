f176127c33285f276a1e8c68651828c7f5f02d01
Alexander Ivanov <alexander.ivanov@ligo.org>
2007-09-14 21:39:48 +0000

    *** empty log message ***
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@754 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Rio1.pm
@@ -0,0 +1,67 @@
+package CDS::Rio1;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Rio1;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        ;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+        ;
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $card =  substr($::partInput[$i][$j], 4, 1);
+        return "rioInput1\[" . $card . "\]";
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+        my $rioNum = substr($::partName[$i], 4, 1);
+        my $calcExp = "// Rio1 (IIRO-16) number is $rioNum name $::partName[$i]\n";
+        my $fromType = $::partInputType[$i][$_];
+        if (($fromType ne "GROUND") && ($::partInput[$i][0] ne "NC")) {
+                $calcExp .= "rioOutput1\[";
+                $calcExp .= $rioNum;
+                $calcExp .= "\] = ";
+                $calcExp .= $::fromExp[0];
+                $calcExp .= ";\n";
+        }
+        return $calcExp;
+
+        return "";
+}
