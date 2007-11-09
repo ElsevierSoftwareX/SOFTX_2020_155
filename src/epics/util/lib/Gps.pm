4918fd9e588d407992d3b1a3a752444bf80d4d2e
Alexander Ivanov <alexander.ivanov@ligo.org>
2007-11-09 21:26:16 +0000

    *** empty log message ***
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@810 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Gps.pm
@@ -0,0 +1,62 @@
+package CDS::Gps;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Gps;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        ;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+        ;
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+ 	#if ( !$::gps_vars_printed) {
+		#$::gps_vars_printed = 1;
+		#print ::OUT "unsigned int cycle_gps_ns;\n";
+ 		#print ::OUT "double cycle_gps_time = getGpsTime(&cycle_gps_ns);\n";
+	#}
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $port = $::partInputPort[$i][$j];
+
+	if ($port == 0) { return "cycle_gps_time"; }
+        elsif ($port == 1) { return "(unsigned long)cycle_gps_time"; }
+        elsif ($port == 2) { return "cycle_gps_time - (unsigned long)cycle_gps_time"; }
+        elsif ($port == 3) { return "cycle_gps_ns"; }
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+        return "";
+}
