3d489b58fdb20f06aaeed58c312c4f9e833e84a9
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2008-06-17 16:39:02 +0000

    Supports cdsIPCS part.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@883 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/IPCS.pm
@@ -0,0 +1,81 @@
+package CDS::IPCS;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return IPCS;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        ;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+        ;
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+	my $partName = $::partInput[$i][$j];
+	#print "IPCS $partName\n";
+	die "IPCS Part $partName invalid: its name must be the hex address\n" unless
+		$partName =~ /^.*0x(\d|[abcdefABCDEF])+$/;
+	my $addressString = $partName;
+	$addressString =~ s/^.*(0x(\d|[abcdefABCDEF])+)$/\1/g;
+        my $address =  hex $partName;
+	if ($address % 4 != 0) {
+		die "IPCS Part $::xpartName[$i] invalid: address must be 4-byte aligned\n";
+	}
+        return "_ipc_shm? *((float *)(((void *)_ipc_shm) + $addressString))- cycle : 0.0";
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+	my $partName = $::xpartName[$i];
+	# print "IPCS Address is $partName\n";
+	die "RfmIO Part $partName invalid: its name must be the hex address\n" unless
+		$partName =~ /^.*0x(\d|[abcdefABCDEF])+$/;
+	my $addressString = $partName;
+	$addressString =~ s/^.*(0x(\d|[abcdefABCDEF])+)$/\1/g;
+	my $address = hex $partName;
+	if ($address % 4 != 0) {
+		die "RfmIO Part $::xpartName[$i] invalid: address must be 4-byte aligned\n";
+	}
+        my $fromType = $::partInputType[$i][$_];
+        if (($fromType ne "GROUND") && ($::partInput[$i][0] ne "NC") && ($fromType ne "")) {
+		return "if (_ipc_shm != 0) {\n"
+        		. "  // IPCS output\n"
+                	. "  *((float *)(((char *)_ipc_shm) + $addressString)) = cycle;\n"
+			. "}\n";
+        }
+        return " ";
+}
