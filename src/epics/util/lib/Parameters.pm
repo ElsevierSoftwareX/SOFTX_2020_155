0c1f8f5efb88c3fb76d07e543cb0701ef89a63d3
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2008-08-21 23:46:00 +0000

    Modified to handle separate DAC upsampling argument.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@917 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/Parameters.pm
+++ src/epics/util/lib/Parameters.pm
@@ -52,6 +52,9 @@ sub printHeaderStruct {
 			} elsif ($spp[0] eq "gds_node_id") {
 				print "GDS node id is set to $spp[1]\n";
 				$::gdsNodeId = $spp[1];
+			} elsif ($spp[0] eq "dacOver") {
+				print "DAC oversample is set to $spp[1]\n";
+				$::dacOver = $spp[1];
 			} elsif ($spp[0] eq "plant_name") {
 				print "Plant name is set to $spp[1]\n";
 				$::plantName = $spp[1];
