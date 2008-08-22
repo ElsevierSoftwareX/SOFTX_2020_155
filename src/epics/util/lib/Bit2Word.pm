418252ae8893c45b49285d322120a11e3b48fe40
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2008-08-22 18:10:35 +0000

    Bit converters.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@921 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Bit2Word.pm
@@ -0,0 +1,70 @@
+package CDS::Bit2Word;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Bit2Word;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "unsigned int \L$::xpartName[$i];\n";
+        print ::OUT "unsigned int powers_of_2[16] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512,\n";
+        print ::OUT "                                1024, 2048, 4096, 8192, 16384, 32768};\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        return "\L$::xpartName[$from]";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+	return "\L$::xpartName[$i] = 0;\n";
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+        my $calcExp = "// Bit2Word\n{\n";
+        $calcExp .= "double ins[$::partInCnt[$i]] = {\n";
+        for (0 .. $::partInCnt[$i]-2) {
+           $calcExp .= "\t$::fromExp[$_],\n";
+        }
+        $calcExp .= "\t$::fromExp[$partInCnt[$i]-1]\n";
+        $calcExp .= "};\n";
+        $calcExp .= "\L$::xpartName[$i] = 0;\n";
+        $calcExp .= "for (ii = 0; ii < 16; ii++)\n{\n";
+        $calcExp .= "if (ins[ii]) {\n";
+        $calcExp .= "\L$::xpartName[$i] += powers_of_2\[ii\];\n";
+        $calcExp .= "}\n";
+        $calcExp .= "}\n";
+        $calcExp .= "}\n";
+	return $calcExp;
+}
