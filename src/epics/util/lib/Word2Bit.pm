2a1f5aed4df0b8eaf227b720558b69bc5314851b
Alexander Ivanov <alexander.ivanov@ligo.org>
2008-08-22 18:10:35 +0000

    Bit converters.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@921 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Word2Bit.pm
@@ -0,0 +1,66 @@
+package CDS::Word2Bit;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Word2Bit;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "unsigned int \L$::xpartName[$i]\[16\];\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        my $fromPort = $::partInputPort[$i][$j];
+        return "\L$::xpartName[$from]\[" . $fromPort . "\]";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+#       my ($i) = @_;
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+        my $calcExp = "// Word2Bit\n{\n";
+        $calcExp .= "unsigned int in = (int) $::fromExp[0];\n";
+        $calcExp .= "for (ii = 0; ii < 16; ii++)\n{\n";
+        $calcExp .= "if (in%2) {\n";
+        $calcExp .= "\L$::xpartName[$i]\[ii\] = 1;\n";
+        $calcExp .= "}\n";
+        $calcExp .= "else {\n";
+        $calcExp .= "\L$::xpartName[$i]\[ii\] = 0;\n";
+        $calcExp .= "}\n";
+        $calcExp .= "in = in>>1;\n";
+        $calcExp .= "}\n";
+        $calcExp .= "}\n";
+	return $calcExp;
+}
