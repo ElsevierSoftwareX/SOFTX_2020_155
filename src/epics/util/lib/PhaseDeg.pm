74323bab58117b1ab1f9097cf551fb39db00c8b5
Alexander Ivanov <alexander.ivanov@ligo.org>
2008-10-14 02:54:52 +0000

    Developed by Sam Waldman, a phase shifter that uses degrees as input.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@940 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/PhaseDeg.pm
@@ -0,0 +1,76 @@
+package CDS::PhaseDeg;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return PhaseDeg;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        print ::OUTH "\tfloat $::xpartName[$i]\[2\];\n";
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+	print ::EPICS "PHASEDEG $::xpartName[$i] $::systemName\.$::xpartName[$i] float ai 0 field(PREC,\"3\")\n";
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "static double \L$::xpartName[$i]\[2\];\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        my $fromPort = $::partInputPort[$i][$j];
+        return "\L$::xpartName[$from]\[" . $fromPort . "\]";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+	return "";
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+	my $calcExp = "// PHASEDEG\n";
+        $calcExp .= "\L$::xpartName[$i]\[0\] = \(";
+        $calcExp .=  "$::fromExp[0]";
+        $calcExp .=  " * ";
+        $calcExp .=  "pLocalEpics->$::systemName\.$::xpartName[$i]";
+        $calcExp .=  "\[1\]\) + \(";
+        $calcExp .=  "$::fromExp[1]";
+        $calcExp .=  " * ";
+        $calcExp .=  "pLocalEpics->$::systemName\.$::xpartName[$i]";
+        $calcExp .=  "\[0\]);\n";
+        $calcExp .= "\L$::xpartName[$i]\[1\] = \(";
+        $calcExp .=  "$::fromExp[1]";
+        $calcExp .=  " * ";
+        $calcExp .=  "pLocalEpics->$::systemName\.$::xpartName[$i]";
+        $calcExp .=  "\[1\]\) - \(";
+        $calcExp .=  "$::fromExp[0]";
+        $calcExp .=  " * ";
+        $calcExp .=  "pLocalEpics->$::systemName\.$::xpartName[$i]";
+        $calcExp .=  "\[0\]);\n";
+        return $calcExp;
+}
