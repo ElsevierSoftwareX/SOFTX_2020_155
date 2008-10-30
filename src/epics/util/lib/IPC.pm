c5e2d5609dd3e479d8c73873ef4cd91612ace4f7
Alexander Ivanov <alexander.ivanov@ligo.org>
2008-10-30 01:16:42 +0000

    modified code generator to do all IPC input in the very beginning on a cycle and to do all IPC output at the very end of a cycle.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@943 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/IPC.pm
+++ src/epics/util/lib/IPC.pm
@@ -24,7 +24,20 @@ sub printEpics {
 # Current part number is passed as first argument
 sub printFrontEndVars  {
         my ($i) = @_;
-        ;
+	my $partName = $::xpartName[$i];
+	die "IPC Part $partName invalid: its name must be the hex address\n" unless
+		$partName =~ /^.*0x(\d|[abcdefABCDEF])+$/;
+	my $addressString = $partName;
+	$addressString =~ s/^.*(0x(\d|[abcdefABCDEF])+)$/\1/g;
+        my $address =  hex $partName;
+	if ($address % 8 != 0) {
+		die "IPC Part $::xpartName[$i] invalid: address must be 8-byte aligned\n";
+	}
+	if ($::partOutCnt[$i] > 0) {
+          print ::OUT "double ipc_at_$addressString = *((double *)(((void *)_ipc_shm) + $addressString));\n";
+	} else {
+          print ::OUT "double ipc_at_$addressString;\n";
+	}
 }
 
 # Return front end initialization code
@@ -48,10 +61,11 @@ sub fromExp {
 	my $addressString = $partName;
 	$addressString =~ s/^.*(0x(\d|[abcdefABCDEF])+)$/\1/g;
         my $address =  hex $partName;
-	if ($address % 4 != 0) {
-		die "IPC Part $::xpartName[$i] invalid: address must be 4-byte aligned\n";
+	if ($address % 8 != 0) {
+		die "IPC Part $::xpartName[$i] invalid: address must be 8-byte aligned\n";
 	}
-        return "*((float *)(((void *)_ipc_shm) + $addressString))";
+#        return "*((double *)(((void *)_ipc_shm) + $addressString))";
+	return "ipc_at_$addressString";
 }
 
 # Return front end code
@@ -67,15 +81,17 @@ sub frontEndCode {
 	my $addressString = $partName;
 	$addressString =~ s/^.*(0x(\d|[abcdefABCDEF])+)$/\1/g;
 	my $address = hex $partName;
-	if ($address % 4 != 0) {
-		die "RfmIO Part $::xpartName[$i] invalid: address must be 4-byte aligned\n";
+	if ($address % 8 != 0) {
+		die "RfmIO Part $::xpartName[$i] invalid: address must be 8-byte aligned\n";
 	}
         my $fromType = $::partInputType[$i][$_];
         if (($fromType ne "GROUND") && ($::partInput[$i][0] ne "NC") && ($fromType ne "")) {
-		return "if (_ipc_shm != 0) {\n"
-        		. "  // IPC output\n"
-                	. "  *((float *)(((char *)_ipc_shm) + $addressString)) = $::fromExp[0];\n"
-			. "}\n";
+#		return "if (_ipc_shm != 0) {\n"
+#        		. "  // IPC output\n"
+#                	. "  *((double *)(((char *)_ipc_shm) + $addressString)) = $::fromExp[0];\n"
+#			. "}\n";
+		$::ipcOutputCode .= "      *((double *)(((char *)_ipc_shm) + $addressString)) = ipc_at_$addressString;\n";
+		return "ipc_at_$addressString = $::fromExp[0];\n";
         }
         return " ";
 }
