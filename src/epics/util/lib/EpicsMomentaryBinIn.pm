6d01c960f991247cb7a0dc2f0b5e5fe2b3a50553
Alexander Ivanov <alexander.ivanov@ligo.org>
2008-12-16 22:09:41 +0000

    EpicsMomentaryBinIn - used by the ISI watchdog code, written by Tobin Fricke. Note that, contrary to the name of this function, it actually specifies an analog input ("ai").  This is just because while debugging I changed it from binary input (bi) to analog input to try to solve a problem that turned out to be unrelated.  We should change it to binary input so that it will actually be what it says it is.  But I don't want to do that now because I know it works in its present implementation, and I don't feel like validating the change at this time.  (TF 2008-12-16)
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@948 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/EpicsMomentaryBinIn.pm
@@ -0,0 +1,55 @@
+package CDS::EpicsMomentaryBinIn;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return EpicsMomentaryBinIn;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        print ::OUTH "\tint $::xpartName[$i];\n";
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+	print ::EPICS "MOMENTARY $::xpartName[$i] $::systemName\.$::xpartName[$i] int ai 0\n";# field(ZNAM,\"OFF\") field(ONAM,\"ON\")\n";
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+        my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        return "pLocalEpics->" . $::systemName . "\." . $::xpartName[$from];
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+        return "";
+}
