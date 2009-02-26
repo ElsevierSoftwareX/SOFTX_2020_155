9c373329d41c81d4e60d91f91ebb19c283df4ee1
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2009-02-26 01:39:23 +0000

    fixed a bug
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@959 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/Rms.pm
+++ src/epics/util/lib/Rms.pm
@@ -56,7 +56,7 @@ sub frontEndCode {
         $calcExp .= $::fromExp[0];
         $calcExp .= ";\n";
         $calcExp .= "if(\L$::xpartName[$i]\ \> 2000\) \L$::xpartName[$i] = 2000;\n";
-        $calcExp .= "if(\L$::xpartName[$i]\ \< -2000\) \L$::xpartName[$i] = 2000;\n";
+        $calcExp .= "if(\L$::xpartName[$i]\ \< -2000\) \L$::xpartName[$i] = -2000;\n";
         $calcExp .= "\L$::xpartName[$i] = \L$::xpartName[$i] * \L$::xpartName[$i];\n";
         $calcExp .= "\L$::xpartName[$i]\_avg = \L$::xpartName[$i] * \.00005 + ";
         $calcExp .= "\L$::xpartName[$i]\_avg * 0\.99995;\n";
