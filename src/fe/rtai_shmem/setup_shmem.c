ffd9859763820f7534e3105bc726da65bb5d46b6
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2009-03-05 19:33:15 +0000

    added IPC memory area allocation code
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@969 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/rtai_shmem/setup_shmem.c
+++ src/fe/rtai_shmem/setup_shmem.c
@@ -10,11 +10,16 @@ MODULE_DESCRIPTION("Create OM1 shared memory");
 MODULE_AUTHOR("Alex Ivanov<aivanov@ligo.caltech.edu>");
 MODULE_LICENSE("GPL");
 
+static char *sysname = "om1";
 #define MMAP_SIZE 1024*1024*64
+
 int init_module() {
-  char *sysname = "om1";
   void *addr = rtai_kmalloc(nam2num(sysname), MMAP_SIZE);
   printk("nam2num(%s)=%d; returned addr = 0x%x\n", sysname, nam2num(sysname), addr);
+  rtai_kmalloc(nam2num("ipc"), MMAP_SIZE);
   return 0;
 }
-void cleanup_module (void) { rtai_kfree(nam2num("om1")); }
+void cleanup_module (void) {
+  rtai_kfree(nam2num(sysname));
+  rtai_kfree(nam2num("ipc"));
+}
