1bbd4f62bc1705359308b095336213ee2b8fa6c3
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2009-03-19 22:22:14 +0000

    allocaste DAQ area
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@985 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/rtai_shmem/setup_shmem.c
+++ src/fe/rtai_shmem/setup_shmem.c
@@ -11,15 +11,22 @@ MODULE_AUTHOR("Alex Ivanov<aivanov@ligo.caltech.edu>");
 MODULE_LICENSE("GPL");
 
 static char *sysname = "om1";
+static char *sysname_daq = "om1_daq";
 #define MMAP_SIZE 1024*1024*64
 
 int init_module() {
+  // Allocate FE to Epics comm area
   void *addr = rtai_kmalloc(nam2num(sysname), MMAP_SIZE);
   printk("nam2num(%s)=%d; returned addr = 0x%x\n", sysname, nam2num(sysname), addr);
-  rtai_kmalloc(nam2num("ipc"), MMAP_SIZE);
+  // Allocate FE DAQ area
+  addr = rtai_kmalloc(nam2num(sysname_daq), MMAP_SIZE);
+  printk("nam2num(%s)=%d; returned addr = 0x%x\n", sysname_daq, nam2num(sysname), addr);
+  // Allocate IPC area
+  //rtai_kmalloc(nam2num("ipc"), MMAP_SIZE);
   return 0;
 }
 void cleanup_module (void) {
   rtai_kfree(nam2num(sysname));
+  rtai_kfree(nam2num(sysname_daq));
   rtai_kfree(nam2num("ipc"));
 }
