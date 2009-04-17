efc70a1176853ff27e0db13e9b7fc12e7df1e65f
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2009-04-17 23:41:53 +0000

    end works, getting weird unsafe RT malloc dmesg...
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1015 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/myriexpress.c
+++ src/fe/myriexpress.c
@@ -8,6 +8,9 @@
 
 #include <drv/cdsHardware.h>
 
+#include "feSelectHeader.h"
+
+
 /*
   Rmote IPC Part will have a remote node name specified (hostname).
   Part will also have remote address specified (e.g. 0, 1, 2). Sizeof double.
@@ -18,12 +21,14 @@
 */
 
 // Defaults came from mx_stream
-#define DFLT_EID   1
 #define FILTER     0x12345
 #define MATCH_VAL  0xabcdef
 
 
-uint32_t board_id = MX_ANY_NIC;
+// This is set to 1 since we also have older Myrinet card here
+// So the new fast Myrinet board is number 1 here
+// This needs to be MX_ANY_NIC if we assume we only have single card
+uint32_t board_id = 1;
 uint32_t filter = FILTER;
 
 // This is the default board rank
@@ -35,11 +40,58 @@ static const uint32_t connection_wait_time = 1000;
 // This node's MX end-point handle
 // All MX communication is done using this handle
 mx_endpoint_t ep;
+int ep_opened = 0;
+
+// MX requests buffers
+// In order to send we need to get an empty request object
+// There will be a thread to go through requests and call mx_test() on them
+// If mx_test() returns success, then the request slot is freed.
+#define MAX_MXR 128
+mx_request_t mxr[MAX_MXR];
+unsigned int  mxr_free[MAX_MXR];
+
+unsigned int cur_mxr;
+
+// Send/receive destinations
+mx_endpoint_addr_t dest[MAX_REMOTE_IPC_HOSTS];
+
+// If the node is connected or not
+unsigned int dest_connected[MAX_REMOTE_IPC_HOSTS];
+
+// Data buffers
+extern unsigned int cds_remote_ipc_nodes;
+//extern unsigned int cds_remote_ipc_size; // Second dimension
+extern double remote_ipc_send[MAX_REMOTE_IPC_HOSTS][MAX_REMOTE_IPC_VARS];
 
 
 // This function will transmit all signal buffers to all nodes we have assigned
 void cds_mx_send() {
-};
+  int i;
+
+  for (i = 0; i < cds_remote_ipc_nodes; i++) {
+    if (!dest_connected[i]) continue;
+
+    if (!mxr_free[cur_mxr]) {
+	mx_status_t   status;
+	uint32_t    result = 0;
+ 	mx_return_t rc = mx_test(ep, &mxr[cur_mxr], &status, &result);
+	if (result == 0) {
+		// Request is still not complete, cancel it
+		mx_cancel(ep, &mxr[cur_mxr], &result);
+	}
+    }
+
+    unsigned int bsize = sizeof(double)*MAX_REMOTE_IPC_VARS; // Buffers size for single host
+    void *data = (void *)&remote_ipc_send[i][0];
+    mx_ksegment_t seg;
+    seg.segment_ptr = data;
+    seg.segment_length =  bsize;
+    mx_kisend(ep, &seg, 1, MX_PIN_KERNEL, dest[i], MATCH_VAL, NULL, &mxr[cur_mxr]);
+    printk("send MX stuff\n");
+    mxr_free[cur_mxr] = 0;
+    cur_mxr++;
+  };
+}
 
 // This one will do the receive (if any); This will really only initiate a DMA into 
 // receive buffers
@@ -49,6 +101,14 @@ void cds_mx_rcv() {
 int cds_mx_init () {
   int i;
 
+  cur_mxr = 0;
+
+  for (i = 0; i < MAX_MXR; i++) {
+  	mxr_free[i] = 1;
+  }
+  for (i = 0; i < MAX_REMOTE_IPC_HOSTS; i++) {
+	dest_connected[i] = 0;
+  }
   mx_init();
   extern CDS_REMOTE_NODES remote_nodes[];
   extern unsigned int cds_remote_ipc_nodes;
@@ -65,10 +125,10 @@ int cds_mx_init () {
      return 1;
   }
   printk("Opened MX endpoint\n");
+  ep_opened = 1;
 
   // Connect to all nodes
   for (i = 0; i < cds_remote_ipc_nodes; i++) {
-    mx_endpoint_addr_t dest;
     uint64_t his_nic_id;
     uint16_t his_eid = remote_nodes[i].port;
     mx_return_t ret;
@@ -80,20 +140,21 @@ int cds_mx_init () {
 	printk("Mx host \"%s\" not found\n", remote_nodes[i].nodename);
   	continue;
     }
-    ret = mx_connect(ep, his_nic_id, his_eid, filter, connection_wait_time, &dest);
+    ret = mx_connect(ep, his_nic_id, his_eid, filter, connection_wait_time, &dest[i]);
     if (ret != MX_SUCCESS) {
-	printk("Mx connect failed to node \"%s\", %s\n", node, mx_strerror(ret));
+	printk("Mx connect failed to node \"%s\" port %d, %s\n", node, his_eid, mx_strerror(ret));
 	continue;
     } else {
 	printk("Sucessfully connected to node %s\n", node);
+	dest_connected[i] = 1;
     }
   }
-
-  cds_mx_finalize();
 }
 
 void cds_mx_finalize() {
-  mx_close_endpoint(ep);
+  printk("Closing MX\n");
+  if (ep_opened) mx_close_endpoint(ep);
+  ep_opened = 0;
   mx_finalize();
 }
 
