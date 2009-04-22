dcc506c1f284aab759b81789c42a2633bdb7ea6f
Alexander Ivanov <alexander.ivanov@ligo.org>
2009-04-22 22:11:09 +0000

    Added some map function defines.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1024 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/drv/map.h
+++ src/include/drv/map.h
@@ -3,17 +3,25 @@
 
 int mapPciModules(CDS_HARDWARE *);/* Init routine to map adc/dac cards*/
 int gsaAdcTrigger(int,int[]);	/* Starts ADC acquisition.	*/
+int gsaDacTrigger(int);		/* Starts DACC acquisition.	*/
 int gsaAdcStop(void);		/* Stop acquistion		*/
 int adcDmaDone(int,int *);	/* Checks if ADC DMA complete.	*/
 int dacDmaDone(int);
-int checkAdcRdy(int,int);	/* Checks if ADC has samples avail */
+int checkAdcRdy(int,int,int);	/* Checks if ADC has samples avail */
 int gsaAdcDma1(int,int,int);	/* Setup ADC DMA registers	*/
 void gsaAdcDma2(int);		/* Send GO bit to ADC DMA registers*/
+int gsaAdcDma3(int, int[]);		/* Send GO bit to ADC DMA registers*/
 int gsaDacDma1(int,int);		/* Setup DAC DMA registers.	*/
 void gsaDacDma2(int,int);	/* Send GO bit to DAC DMA registers.*/
 unsigned int readDio(CDS_HARDWARE *,int);
+unsigned int readIiroDio(CDS_HARDWARE *,int);
+unsigned int readIiroDio1(CDS_HARDWARE *,int);
+unsigned int readCDO32l(CDS_HARDWARE *,int);
 /*int mapRfm(CDS_HARDWARE *pHardware, struct pci_dev *rfmdev);*/
-void writeDio(CDS_HARDWARE *pHardware, int modNum, int data);
+void writeDio(CDS_HARDWARE *, int, int);
+void writeIiroDio(CDS_HARDWARE *, int, int);
+void writeIiroDio1(CDS_HARDWARE *, int, int);
+unsigned int writeCDO32l(CDS_HARDWARE *, int, unsigned int);
 
 #ifdef OVERSAMPLE
 #ifdef SERVO2K
