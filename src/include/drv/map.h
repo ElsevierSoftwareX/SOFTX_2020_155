99882ec6842ffabbbcb3b5a931c6c5f125e18448
aivanov <aivanov@6dcd42c9-f523-4c6d-aada-af552506706e>
2009-09-17 18:58:00 +0000

    Modified to handle 4096 control system rate (RB). Still needs proper down/up sample filters for this rate. Present use is only at LASTI for ISI, which does not down/up sample.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1122 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/drv/map.h
+++ src/include/drv/map.h
@@ -29,6 +29,9 @@ int dacDmaPreload(int, int);
 #ifdef SERVO2K
 #define OVERSAMPLE_TIMES        32
 #define FE_OVERSAMPLE_COEFF     feCoeff32x
+#elif SERVO4K
+#define OVERSAMPLE_TIMES        16
+#define FE_OVERSAMPLE_COEFF     feCoeff32x
 #elif SERVO16K
 #define OVERSAMPLE_TIMES        4
 #define FE_OVERSAMPLE_COEFF     feCoeff4x
