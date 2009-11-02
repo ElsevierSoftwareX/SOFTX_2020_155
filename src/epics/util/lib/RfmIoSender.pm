5ec7b6f9d1133f589b19da3677bc45cb6011d3a2
Alexander Ivanov <alexander.ivanov@ligo.org>
2009-11-02 19:02:20 +0000

    Final RFM Comm changes - M Evans 29 Oct 09
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1186 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/RfmIoSender.pm
+++ src/epics/util/lib/RfmIoSender.pm
@@ -20,120 +20,137 @@ use Exporter;
 # ::OUTH   --> advLigo/src/include/om1.h    - Defines a struct containing storage for all EPICS variables
 # ::EPICS  --> advLigo/src/epics/fmseq/om1  - Some kind of list of EPICS channels?
 
+####################### special helper for RfmIoSender
+# Convert the part name to the RFM address
+#   the name format is "<part name>:<data rate>:<hex address>"
+sub parseName {
+    my $partName = shift(@_);
+    my ($MYNAME, $dataRate, $fullAddress) = split(/x/, $partName);
+
+    # convert full address to card number and address
+    ($fullAddress =~ /^(\d|[abcdefABCDEF])+$/) or
+      die "RfmIoSender $partName invalid: name must be " .
+          " \"part name:data rate:hex address\"\n";
+    $fullAddress =  hex $fullAddress;
+
+    my $cardNum = ($fullAddress - $fullAddress % 0x4000000) / 0x4000000;
+    my $rfmAddress = $fullAddress % 0x4000000;
+
+    ($rfmAddress % 8 == 0) or
+	die "RfmIoSender: address must be 8-byte aligned\n";
+
+    return ($MYNAME, $dataRate, $cardNum, $rfmAddress);
+}
+
+####################### special helper for RfmIoSender
+# Return the names of local variables
+#   MYNAME is first argument
+sub getLocalVars {
+    my $MYNAME = shift(@_);
+    my $MYSTATE = "\L${MYNAME}";
+    my $MYINPUT = "${MYSTATE}_inputValue";
+    my $MYCYCLE = "${MYSTATE}_cycleCount";
+
+    return ($MYSTATE, $MYINPUT, $MYCYCLE);
+}
+
+#####################################################
+
+# return part type name
 sub partType {
-	return RfmIoSender;
+    return RfmIoSender;
 }
 
 # Print Epics communication structure into a header file
 # Current part number is passed as first argument
 sub printHeaderStruct {
-    # This stuff appears in the middle of a struct definition
-    my ($i) = @_;
-    my $MYNAME = $::xpartName[$i];
-    $MYNAME = 'RFMCOMM'; # HACK
-    print ::OUTH <<END;
-int ${MYNAME}\_WAIT;
-int ${MYNAME}\_RESET;
-int ${MYNAME}\_ERRORCOUNT;
-END
+  # no Epics
 }
 
 # Print Epics variable definitions
 # Current part number is passed as first argument
 sub printEpics {
-    my ($i) = @_;
-    my $MYNAME = $::xpartName[$i];
-    $MYNAME = 'RFMCOMM'; # HACK
-    my $SYSNAME = $::systemName;   
-    print ::EPICS <<END;
-MOMENTARY   ${MYNAME}\_WAIT       ${SYSNAME}\.${MYNAME}\_WAIT           int ai 0
-MOMENTARY   ${MYNAME}\_RESET      ${SYSNAME}\.${MYNAME}\_RESET          int ai 0
-OUTVARIABLE ${MYNAME}\_ERRORCOUNT ${SYSNAME}\.${MYNAME}\_ERRORCOUNT     int ao 0
-END
+  # no Epics
 }
 
 # Print variable declarations int front-end file
 # Current part number is passed as first argument
 sub printFrontEndVars  {
-    my ($i) = @_;
-    my $MYNAME = $::xpartName[$i];
-    $MYNAME = 'RFMCOMM'; # HACK
-    my $LOCALSTATE = "\L${MYNAME}";
+    my $partNum = shift(@_);
+    my $partName = $::xpartName[$partNum];
+    my ($MYNAME, $dataRate, $cardNum, $rfmAddress) = parseName($partName);
+    my ($MYSTATE, $MYINPUT, $MYCYCLE) = getLocalVars($MYNAME);
+
+    my $rfmAddressString = sprintf("0x%x", $rfmAddress);
+
     print ::OUT<<END;
+/* RfmIoSender $MYNAME: $dataRate Hz at $rfmAddressString ($cardNum) */
 #define COMMDATA_INLINE
 #include "commData.h"
-static struct CommState ${LOCALSTATE};
+static struct CommDataState ${MYSTATE};
+static cycle_cd ${MYCYCLE}; /* local cycle counter */
+double ${MYINPUT};   /* temporary space for the input value */
+
 END
 }
-
-# Convert the part name to the RFM address
-sub nameToAddress {
-    my ($partName) = @_;
-    die "RfmIO Part $partName invalid: its name must be the hex address\n" unless
-	$partName =~ /^.*0x(\d|[abcdefABCDEF])+$/;
-    my $rfmAddress =  hex $partName;
-    my $card_num = ($rfmAddress - $rfmAddress % 0x4000000) / 0x4000000;
-    $rfmAddress = $rfmAddress % 0x4000000;
-    return $rfmAddress;
-}
-
 
 # Return front end initialization code
 # Argument 1 is the part number
 # Returns calculated code string
 sub frontEndInitCode {
-    my ($i) = @_;
-    my $partName = $::xpartName[$i];
-    print "Reflective Memory Address is $partName\n";
+    my $partNum = shift(@_);
+    my $partName = $::xpartName[$partNum];
+    my ($MYNAME, $dataRate, $cardNum, $rfmAddress) = parseName($partName);
+    my ($MYSTATE, $MYINPUT, $MYCYCLE) = getLocalVars($MYNAME);
 
-    my $rfmBaseAddress = nameToAddress($partName);
-    my $rfmSemaphoreAddress = $rfmBaseAddress;
-    my $rfmDataAddress = $rfmBaseAddress + 4;
+    my $rfmAddressString = sprintf("0x%x", $rfmAddress);
 
-    if ($rfmDataAddress % 4 != 0) {
-	die "RfmIoSynchronizedSender: data address must be 4-byte aligned\n";
-    }
-    if ($rfmDataAddress % 4 != 0) {
-	die "RfmIoSynchronizedSender: semaphore address must be 4-byte aligned\n";
-    }
+    print "RfmIoSender $MYNAME $dataRate Hz at " .
+          "$rfmAddressString ($cardNum)\n";
 
-    my $rfmSemaphoreAddressString = sprintf("0x%x", $rfmSemaphoreAddress);
-    my $rfmDataAddressString = sprintf("0x%x", $rfmDataAddress);
-
-    my $MYNAME = 'RFMCOMM'; # HACK
-    my $LOCALSTATE = "\L${MYNAME}";
-
-    my $card_num = 0; # HACK
-    # Many of these values should really not be hard-coded, but
-    # I don't know how to properly parametrize the block.
+    # return init code
+    #   this assumes one double as input
     return <<END;
-commDataInit(&(${LOCALSTATE}),   
-             COMM_SEND,  /* commType */
-	     2,          /* Nsamp */
-	     (unsigned int *)(((char *)cdsPciModules.pci_rfm[$card_num]) + $rfmSemaphoreAddressString), /* rfmCounter */
-	     (double *)(((char *)cdsPciModules.pci_rfm[$card_num]) + $rfmDataAddressString), /* rfmData */
-	     1);         /* dataLength */
+/* RfmIoSender $MYNAME: $dataRate Hz at $rfmAddressString ($cardNum) */
+
+// init state struct $MYSTATE
+commDataInit(&${MYSTATE}, $dataRate,   // state struct, dataRate
+  sizeof(double), (void*)&${MYINPUT},  // dataBytes, input address
+  (void*)(((char*)cdsPciModules.pci_rfm[$cardNum]) + $rfmAddressString));
+
 END
 }
 
 # Figure out part input code
-# Argument 1 is the part number
-# Argument 2 is the input number
+#   Argument 1 is the part number
+#   Argument 2 is the input number
 # Returns calculated input code
-sub fromExp {
-    my ($i, $j) = @_;
-    my $from = $::partInNum[$i][$j];
-    my $fromPort = $::partInputPort[$i][$j];
-    # return "\L$::xpartName[$from]" . "\[" . $fromPort . "\]";
-    my $MYNAME = 'RFMCOMM'; # HACK
-    my $LOCALSTATE = "\L${MYNAME}";
+#
+# "input code" means the expressing that serves as the input
+# to a part that takes output from this part.  Typically this
+# is just the name of this part's output variable.
+#
+# For example, if the part has an array of output values
+# this would just return "\L$MYNAME.out\[" . $fromPort . "\]";
 
-    if ($fromPort == 0) {
-	return "${LOCALSTATE}.errCounter";
-    } elsif ($fromPort == 1) {
-	return "${LOCALSTATE}.cycleCounter";
+sub fromExp {
+    # destNum and destInput refer to the caller (data destination)
+    my ($destNum, $destInput) = @_;
+
+    # partNum and srcPort refer to this part (data source)
+    my $partNum = $::partInNum[$destNum][$destInput];
+    my $srcPort = $::partInputPort[$destNum][$destInput];
+
+    my $partName = $::xpartName[$partNum];
+    my ($MYNAME, $dataRate, $cardNum, $rfmAddress) = parseName($partName);
+    my ($MYSTATE, $MYINPUT, $MYCYCLE) = getLocalVars($MYNAME);
+
+    # RfmIoSender outputs are:
+    #   cycle counter
+    if ($srcPort == 0) {
+	return "${MYCYCLE}";
     } else {
-	die "RfmIoSynchronizedSender has too many outputs";
+	die "RfmIoSender $MYNAME has too many outputs (4 max)";
     }
 }
 
@@ -142,32 +159,34 @@ sub fromExp {
 # Returns calculated code string
 
 sub frontEndCode {
-    my ($i) = @_;
-    print "frontEndCode($i)\n";
-    my $partName = $::xpartName[$i];
-    my $fromType = $::partInputType[$i][$_];
+    my $partNum = shift(@_);
+    print "frontEndCode($partNum)\n";
 
-    my $MYNAME = $::xpartName[$i];
-    $MYNAME = 'RFMCOMM'; # HACK
-    my $SYSNAME = $::systemName;   
-    my $LOCALSTATE = "\L${MYNAME}"; # name of CommStruct
+    my $partName = $::xpartName[$partNum];
+    my ($MYNAME, $dataRate, $cardNum, $rfmAddress) = parseName($partName);
+    my ($MYSTATE, $MYINPUT, $MYCYCLE) = getLocalVars($MYNAME);
 
+    my $rfmAddressString = sprintf("0x%x", $rfmAddress);
+
+    # input expressions
     my $INPUT =  $::fromExp[0];
-    my $RESET = "pLocalEpics->${SYSNAME}\.${MYNAME}_RESET";
-    my $WAIT  = "pLocalEpics->${SYSNAME}\.${MYNAME}_WAIT";
-    my $ERRCOUNT = "pLocalEpics->${SYSNAME}\.${MYNAME}_ERRORCOUNT";
-   
+
     return <<END;
-if ($RESET) {
-  ${LOCALSTATE}.errCounter = 0;
-  $RESET = 0;
+// ==============================================================
+// RfmIoSender $MYNAME: $dataRate Hz at $rfmAddressString ($cardNum)
+
+// HACK: write only on even cycles (should be odd)
+if( !(cycle & 1) )
+{
+  // set cycle counter
+  //  int cycle is an argument to feCode, counting at FE_RATE
+  $MYCYCLE = cycle * (COMMDATA_MAX_RATE / FE_RATE);
+
+  // copy data to shared memory
+  $MYINPUT = $INPUT;
+  commDataSend(&${MYSTATE}, $MYCYCLE);
 }
-if ($WAIT) {
-  ${LOCALSTATE}.waitCounter ++;
-  $WAIT = 0;
-}
-commDataRecv(&${LOCALSTATE}, &(${INPUT})); /* FIXME: assuming that "${INPUT}" is something I can take address-of! */
-${ERRCOUNT} = ${LOCALSTATE}.errCounter;
+
 END
 
 }
