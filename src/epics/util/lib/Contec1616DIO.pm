79939f1fdcca5defeeea9ac1321ce7524073b814
Alexander Ivanov <alexander.ivanov@ligo.org>
2009-12-15 20:31:45 +0000

    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1200 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Contec1616DIO.pm
@@ -0,0 +1,90 @@
+package CDS::Contec1616DIO;
+use Exporter;
+@ISA = ('Exporter');
+
+sub initCDIO1616 {
+	my ($node) = @_;
+	$::boPartNum[$::boCnt] = $::partCnt;
+	my $desc = ${$node->{FIELDS}}{"Name"};
+	my $l = length($desc);
+        my $CDIO1616num = substr($desc, ($l-1), 1);
+        if ($CDIO1616num =~ m/\D/) {
+           die "Last character of module name must be digit\: $desc\n";
+        } 
+	$::boType[$::boCnt] = "CON_1616DIO";
+	$::boNum[$::boCnt] = $CDIO1616num;
+	$::boCnt ++;
+}
+
+sub partType {
+	return Contec1616DIO;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        ;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+        ;
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+	my $l = length($::partInput[$i][$j]);
+        my $card =  substr($::partInput[$i][$j], ($l-1), 1);
+	if ($::partInputPort[$i][$j] == 0)
+	{
+        	return "(CDIO1616Input\[" . $card . "\] >> 16)";
+	} else {
+        	return "(CDIO1616Input\[" . $card . "\] & 0xffff)";
+	}
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+	my $l = length($::partName[$i]);
+        my $CDIO1616Num = substr($::partName[$i], ($l-1), 1);
+        my $calcExp = "// CDIO1616 number is $CDIO1616Num name $::partName[$i]\n";
+        my $fromType = $::partInputType[$i][$_];
+        if (($fromType ne "GROUND") && ($::partInput[$i][0] ne "NC")) {
+                $calcExp .= "CDIO1616Output\[";
+                $calcExp .= $CDIO1616Num;
+                $calcExp .= "\] = ((int)";
+                $calcExp .= $::fromExp[0];
+                $calcExp .= " << 16) + ((int)";
+                $calcExp .= $::fromExp[0];
+		$calcExp .= " & 0xffff);\n";
+        }
+        return $calcExp;
+
+        return "";
+}
