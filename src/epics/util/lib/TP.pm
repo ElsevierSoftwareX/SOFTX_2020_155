219e0c725ee632d2db9c6bbc83f2ce2c3a23fbe5
alexander.ivanov@LIGO.ORG <alexander.ivanov@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2010-02-02 22:55:14 +0000

    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1216 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/TP.pm
@@ -0,0 +1,62 @@
+package CDS::TP;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return TP;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+	$::extraTPcount ++;
+	$::extraTestPoints .= " $::xpartName[$i]";
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+	print ::OUT "static float \L$::xpartName[$i];\n";
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+        my ($i) = @_;
+	my $tpn = $::extraTPcount;
+	$ret = "testpoint[$tpn] = &\L$::xpartName[$i];\n";
+	$ret .= "\L$::xpartName[$i] = .0;\n";
+        return $ret;
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+	return ""; #\L$::xpartName[$i];
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+        my $ret = "// Extra Test Point\n";
+	$ret .= "\L$::xpartName[$i]";
+        $ret .= " = $::fromExp[0];\n";
+	return $ret;
+}
