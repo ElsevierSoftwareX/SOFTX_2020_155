5acb0e3e6777b59ba91aee1f9bacbb20c450bc82
alexander.ivanov@LIGO.ORG <alexander.ivanov@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2010-06-01 17:34:35 +0000

    New white noise part
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1915 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/Noise.pm
@@ -0,0 +1,56 @@
+package CDS::Noise;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return Noise;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "static double \L$::xpartName[$i];\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        return "\L$::xpartName[$from]";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+        my $calcExp =  "\L$::xpartName[$i] = 0;\n";
+	return $calcExp;
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+        my $calcExp = "// Noise\n";
+        $calcExp .= "\L$::xpartName[$i] = 0;\n;";
+}
