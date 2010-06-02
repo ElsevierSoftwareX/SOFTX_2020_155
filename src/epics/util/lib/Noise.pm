6c536f1a226ca5c175b087a6f46527ff8ec4e021
Alexander Ivanov <alexander.ivanov@ligo.org>
2010-06-02 23:31:28 +0000

    Added noise generator code (chapter 7.1 Numerical Recipes, Third Edition)
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1923 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/Noise.pm
+++ src/epics/util/lib/Noise.pm
@@ -2,6 +2,10 @@ package CDS::Noise;
 use Exporter;
 @ISA = ('Exporter');
 
+$printed = 0;
+$init_code_printed = 0;
+1;
+
 sub partType {
 	return Noise;
 }
@@ -23,7 +27,29 @@ sub printEpics {
 # Current part number is passed as first argument
 sub printFrontEndVars  {
         my ($i) = @_;
-        print ::OUT "static double \L$::xpartName[$i];\n";
+	print ::OUT "static double \L$::xpartName[$i];\n";
+	if ($printed) { return; }
+	print ::OUT << "END";
+static unsigned long noise_seed = 4101842887655102017LL;\n
+static unsigned long noise_u, noise_v, noise_w;
+inline unsigned long noise_int64() {
+   noise_u = noise_u * 2862933555777941757LL + 7046029254386353087LL;
+   noise_v ^= noise_v >> 17; noise_v ^= noise_v << 31; noise_v ^= noise_v >> 8;
+   noise_w = 4294957665U * (noise_w & 0xffffffff) + (noise_w >> 32);
+   unsigned long noise_x = noise_u ^ (noise_u << 21);
+   noise_x ^= noise_x >> 35; noise_x ^= noise_x <<4;
+   return (noise_x + noise_v) ^ noise_w;
+}
+inline double noise_doub() { return 5.42101086242752217E-20 * noise_int64(); }
+inline noise_ran(unsigned long j) {
+   noise_v = 4101842887655102017LL;
+   noise_w = 1;
+   noise_u = j ^ noise_v; noise_int64();
+   noise_v = noise_u; noise_int64();
+   noise_w = noise_v; noise_int64();
+}
+END
+$printed = 1;
 }
 
 # Figure out part input code
@@ -41,7 +67,12 @@ sub fromExp {
 # Returns calculated code string
 sub frontEndInitCode {
 	my ($i) = @_;
-        my $calcExp =  "\L$::xpartName[$i] = 0;\n";
+	if ($init_code_printed) { return ""; }
+        my $calcExp  =  "\L$::xpartName[$i] = 0;\n";
+	$calcExp    .=  "for (;noise_seed == 4101842887655102017LL;) {\n";
+        $calcExp    .=  "   rdtscl(noise_seed);\n";
+        $calcExp    .=  "}\n";
+	$init_code_printed = 1;
 	return $calcExp;
 }
 
@@ -52,5 +83,5 @@ sub frontEndInitCode {
 sub frontEndCode {
 	my ($i) = @_;
         my $calcExp = "// Noise\n";
-        $calcExp .= "\L$::xpartName[$i] = 0;\n;";
+        $calcExp .= "\L$::xpartName[$i] = noise_doub();\n";
 }
