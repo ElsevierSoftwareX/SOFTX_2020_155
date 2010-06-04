af41414c4dc6c940ab002f80d7a40f01b9a06104
alexander.ivanov@LIGO.ORG <alexander.ivanov@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2010-06-04 22:55:00 +0000

    these directories are automatically created by the build process and they should not be checked into the SVN
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@1943 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/pde/LSC_prc_inv.c
+++
@@ -1,8 +0,0 @@
-void LSC_prc_inv(double *in, int inSize, double *out, int outSize)
-{
-if(in[2] > in[0])
-	out[0] = in[1]*-1;
-else
-	out[0] = in[1];
-
-}
--- src/fe/pde/LSC_shutter.c
+++
@@ -1,8 +0,0 @@
-int curcycle;
-void LSC_shutter(double *in, int inSize, double *out, int outSize)
-{
-if(in[2] == 0 || in[0] > in[1])
-	out[0] = 1; // closed
-else
-	out[0] = 0; // open
-}
