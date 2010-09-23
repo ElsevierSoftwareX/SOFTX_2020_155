19900a8e13ed7f1cb4be38ffe0a85fe6f8f6e7b7
Rolf Bork <rolf.bork@ligo.org>
2010-09-23 20:09:14 +0000

    Made structure for new Symmetricom IRIG-B PCIe cards.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2090 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/drv/cdsHardware.h
+++ src/include/drv/cdsHardware.h
@@ -488,6 +488,28 @@ typedef struct GSA_AD18_REG{
 
 // Symmertricom GPS input card
 // model BC635PCI-U
+typedef struct SYMCOM_REGISTER{
+	unsigned int TIMEREQ;
+	unsigned int EVENTREQ;
+	unsigned int UNLOCK1;
+	unsigned int UNLOCK2;
+	unsigned int CONTROL;
+	unsigned int ACK;
+	unsigned int MASK;
+	unsigned int INTSTAT;
+	unsigned int MINSTRB;
+	unsigned int MAJSTRB;
+	unsigned int EVENT2_0;
+	unsigned int EVENT2_1;
+	unsigned int TIME0;
+	unsigned int TIME1;
+	unsigned int EVENT0;
+	unsigned int EVENT1;
+	unsigned int RESERV1;
+	unsigned int UNLOCK3;
+	unsigned int EVENT3_0;
+	unsigned int EVENT3_1;
+}SYMCOM_REGISTER;
 
 #define SYMCOM_VID		0x12e2
 #define SYMCOM_BC635_TID	0x4013
