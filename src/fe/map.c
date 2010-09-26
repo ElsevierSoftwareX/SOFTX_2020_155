0ad7bc2b14eb6f9c474b92f3029395d4d6498492
rolf.bork@LIGO.ORG <rolf.bork@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2010-09-26 18:05:52 +0000

    Modified to support Symmetricom IRIG-B card in IEEE-1344 and DCLS mode, required for use with Daniel's IRIG-B fanout unit.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2097 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe/map.c
+++ src/fe/map.c
@@ -1425,18 +1425,104 @@ int mapSymComGps(CDS_HARDWARE *pHardware, struct pci_dev *gpsdev)
   static unsigned int pci_io_addr;
   int pedStatus;
   unsigned char *addr1;
+  unsigned char *addr3;
+  unsigned char *addr4;
+  unsigned char *addr5;
+  unsigned int *cmd;
+  unsigned int *dramRead;
   unsigned int time0;
+  unsigned char inPut[8];
+  SYMCOM_REGISTER *timeReg;
 
   pedStatus = pci_enable_device(gpsdev);
   pci_read_config_dword(gpsdev, PCI_BASE_ADDRESS_2, &pci_io_addr);
   pci_io_addr &= 0xfffffff0;
-  printk("PIC BASE 0 address = %x\n", pci_io_addr);
+  printk("PIC BASE 2 address = %x\n", pci_io_addr);
 
   addr1 = (unsigned char *)ioremap_nocache((unsigned long)pci_io_addr, 0x40);
   printk("Remapped 0x%p\n", addr1);
   pHardware->gps = addr1;
   pHardware->gpsType = SYMCOM_RCVR;
+  timeReg = (TSYNC_REGISTER *) addr1;;
 
+  pci_read_config_dword(gpsdev, PCI_BASE_ADDRESS_3, &pci_io_addr);
+  pci_io_addr &= 0xfffffff0;
+  addr3 = (unsigned char *)ioremap_nocache((unsigned long)pci_io_addr, 0x200);
+  printk("PIC BASE 3 address = 0x%x\n", pci_io_addr);
+  printk("PIC BASE 3 address = 0x%p\n", addr3);
+  dramRead = (unsigned int *)(addr3 + 0x82);
+  cmd = (unsigned int *)(addr3 + 0x102);
+  //
+  // Set write and wait *****************************
+  *cmd = 0xf6; // Request model ID
+	i=0;
+        timeReg->ACK = 0x1;  // Trigger module to capture time
+	udelay(1000);
+        timeReg->ACK = 0x80;  // Trigger module to capture time
+	do{
+	udelay(1000);
+	i++;
+	}while((timeReg->ACK == 0) &&(i<20));
+	if(timeReg->ACK) printk("SysCom ack received ID %d !!! 0x%x\n",timeReg->ACK,i);
+	printk("Model = 0x%x\n",*dramRead);
+// End Wait ****************************************
+  //
+  // Set write and wait *****************************
+	*cmd = 0x4915; // Request model ID
+	i=0;
+        timeReg->ACK = 0x1;  // Trigger module to capture time
+	udelay(1000);
+        timeReg->ACK = 0x80;  // Trigger module to capture time
+	do{
+	udelay(1000);
+	i++;
+	}while((timeReg->ACK == 0) &&(i<20));
+	if(timeReg->ACK) printk("SysCom ack received ID %d !!! 0x%x\n",timeReg->ACK,i);
+	printk("Model = 0x%x\n",*dramRead);
+// End Wait ****************************************
+  //
+  // Set write and wait *****************************
+	*cmd = 0x4416; // Request model ID
+	i=0;
+        timeReg->ACK = 0x1;  // Trigger module to capture time
+	udelay(1000);
+        timeReg->ACK = 0x80;  // Trigger module to capture time
+	do{
+	udelay(1000);
+	i++;
+	}while((timeReg->ACK == 0) &&(i<20));
+	if(timeReg->ACK) printk("SysCom ack received ID %d !!! 0x%x\n",timeReg->ACK,i);
+	printk("Model = 0x%x\n",*dramRead);
+// End Wait ****************************************
+  //
+  // Set write and wait *****************************
+	*cmd = 0x1519; // Request model ID
+	i=0;
+        timeReg->ACK = 0x1;  // Trigger module to capture time
+	udelay(1000);
+        timeReg->ACK = 0x80;  // Trigger module to capture time
+	do{
+	udelay(1000);
+	i++;
+	}while((timeReg->ACK == 0) &&(i<20));
+	if(timeReg->ACK) printk("SysCom ack received ID %d !!! 0x%x\n",timeReg->ACK,i);
+	printk("New Time COde Format = 0x%x\n",*dramRead);
+// End Wait ****************************************
+  //
+  // Set write and wait *****************************
+	*cmd = 0x1619; // Request model ID
+	i=0;
+        timeReg->ACK = 0x1;  // Trigger module to capture time
+	udelay(1000);
+        timeReg->ACK = 0x80;  // Trigger module to capture time
+	do{
+	udelay(1000);
+	i++;
+	}while((timeReg->ACK == 0) &&(i<20));
+	if(timeReg->ACK) printk("SysCom ack received ID %d !!! 0x%x\n",timeReg->ACK,i);
+	printk("New TC Modulation = 0x%x\n",*dramRead);
+// End Wait ****************************************
+	
   for (i = 0; i < 10; i++) {
     pHardware->gps[0] = 1;
     printk("Current time %ds %dus %dns \n", (pHardware->gps[0x34/4]-252806386), 0xfffff & pHardware->gps[0x30/4], 100 * ((pHardware->gps[0x30/4] >> 20) & 0xf));
