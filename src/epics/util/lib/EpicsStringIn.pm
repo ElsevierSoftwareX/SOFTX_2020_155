f69f0d66e3a0684a83c0fc3501495df45b7165b6
John Aronsson <john.aronsson@ligo.org>
2010-10-26 21:25:05 +0000

    Added two EPICS .pm files
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2139 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/EpicsStringIn.pm
@@ -0,0 +1,55 @@
+package CDS::EpicsStringIn;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return EpicsStringIn;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+#       print ::OUTH "\tfloat $::xpartName[$i];\n";
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+        print ::EPICS "DUMMY $::xpartName[$i] int stringin 0 \n";
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        ;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+        my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        return "pLocalEpics->" . $::systemName . "\." . $::xpartName[$from];
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+	my ($i) = @_;
+        return "";
+}
