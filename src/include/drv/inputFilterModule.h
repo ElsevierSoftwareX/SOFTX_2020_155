960ef6a24d361257a82d01e74df5211dfa48a130
alexander.ivanov@LIGO.ORG <alexander.ivanov@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2011-01-21 20:22:40 +0000

    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2215 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/drv/inputFilterModule.h
+++ src/include/drv/inputFilterModule.h
@@ -1,10 +1,22 @@
 
+// TODO: write ramping code
+//
+#define M_PI 	3.14159265358979323846
 
-inline void inputFilterModule(double in, double *old_out, double *old_val, double offset, double k, double p, double z)
+inline void inputFilterModule(
+	double in,						/* input IN0 */
+	double *old_out,					/* input IN1_PREV, output IN1 */
+	double *old_val,					/* input VAL_PREV, output VAL */
+	double offset,						
+	double *pk, double *pp, double *pz,			/* input current value, output new values (after ramping was done */
+	double epics_k, double epics_p, double epics_z,		/* EPICS record values, their change initiates ramping */
+	double k_tramp, double p_tramp, double z_tramp)		/* EPICS records, ramp times in seconds */
 {
+	double p = *pp * M_PI/CYCLE_PER_SECOND;
 	double a = (1.0 - p) / (1.0 + p);
+	double z = *pz * M_PI/CYCLE_PER_SECOND;
 	double b = (1.0 - z) / (1.0 + z);
-	double newval = k * (in + offset);
+	double newval = *pk * (in + offset);
 	double out = newval - b * *old_val + a * *old_out;
 	*old_out = out;
 	*old_val = newval;
