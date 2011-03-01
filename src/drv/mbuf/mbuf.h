859d2421a78a4ff80ba0a4b5ae0902132a04a659
alexander.ivanov@LIGO.ORG <alexander.ivanov@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2011-03-01 21:51:52 +0000

    fixed non-op deallocate ioctl
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2308 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/drv/mbuf/mbuf.h
+++ src/drv/mbuf/mbuf.h
@@ -2,17 +2,18 @@
 // Maximum length of memory buffer tag
 #define MBUF_NAME_LEN 32
 
-// Get memory buffer info
-#define IOCTL_MBUF_INFO 0
-
-// Allocate new or attach to an existing memory buffer with the tag
-#define IOCTL_MBUF_ALLOCATE 1
-
-// Kill a buffer 
-#define IOCTL_MBUF_DEALLOCATE 2
-
 struct mbuf_request_struct{
         size_t size;
         char name[MBUF_NAME_LEN+1];
 };
+
+// Get memory buffer info
+#define IOCTL_MBUF_INFO _IO(0,0)
+
+// Allocate new or attach to an existing memory buffer with the tag
+#define IOCTL_MBUF_ALLOCATE _IOW(0,1,struct mbuf_request_struct)
+
+// Kill a buffer 
+#define IOCTL_MBUF_DEALLOCATE _IOW(0,2,struct mbuf_request_struct)
+
 
