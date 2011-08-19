d8b1e1ef9d34d6a1dc8e284a6840a830d2f4f919
Alexander Ivanov <alexander.ivanov@ligo.org>
2011-08-19 23:16:23 +0000

    added more global externs
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2562 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/fe.h
+++ src/include/fe.h
@@ -1,5 +1,6 @@
 extern int printk(const char *fmt, ...);
 #define printf printk
+#include "linux/types.h"
 #include "inlineMath.h"
 #include "drv/cdsHardware.h"
 #include FE_HEADER
@@ -12,4 +13,6 @@ extern unsigned int CDIO6464InputInput[MAX_DIO_MODULES];
 extern float *testpoint[500];
 extern char *_ipc_shm;
 extern int clock16K;
-
+extern CDS_HARDWARE cdsPciModules;
+extern unsigned int ipcErrBits;
+extern unsigned int timeSec;
