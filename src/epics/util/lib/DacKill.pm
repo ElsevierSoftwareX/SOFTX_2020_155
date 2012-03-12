b18a80a1e037cc0a2632a30214765e389d48fbc1
rolf.bork@LIGO.ORG <rolf.bork@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2012-03-12 18:13:05 +0000

    Part allows models to directly kill DAC outputs in the controller.c code.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@2854 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/DacKill.pm
@@ -0,0 +1,107 @@
+package CDS::DacKill;
+use Exporter;
+@ISA = ('Exporter');
+
+sub partType {
+	return DacKill;
+}
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+	my $MYNAME = $::xpartName[$i];
+	print ::OUTH <<END;
+int $MYNAME\_STATE;
+int $MYNAME\_RESET;
+int $MYNAME\_BPSET;
+int $MYNAME\_BPTIME;
+END
+
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+	print ::EPICS <<END;
+OUTVARIABLE $::xpartName[$i]\_STATE $::systemName\.$::xpartName[$i]\_STATE int ai 0 \n
+MOMENTARY $::xpartName[$i]\_RESET $::systemName\.$::xpartName[$i]\_RESET int ai 0
+MOMENTARY $::xpartName[$i]\_BPSET $::systemName\.$::xpartName[$i]\_BPSET int ai 0
+OUTVARIABLE $::xpartName[$i]\_BPTIME $::systemName\.$::xpartName[$i]\_BPTIME int ai 0 \n
+END
+}
+
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+        print ::OUT "static int \L$::xpartName[$i];\n";
+        print ::OUT "static int \L$::xpartName[$i]_remainingTime;\n";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+        my $from = $::partInNum[$i][$j];
+        my $fromPort = $::partInputPort[$i][$j];
+        return "\L$::xpartName[$from]";
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+	my ($i) = @_;
+	return "";
+}
+
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndCode {
+	my ($i) = @_;
+
+	 my $SIGNAL = $::fromExp[0];
+	 my $BPTIME = $::fromExp[1];
+	 my $DACSTAT = "dacFault";
+	 my $CYCLE = "cycle";
+	 my $STATE = "\L$::xpartName[$i]";
+	 my $BPTIME_REMAINING = "\L$::xpartName[$i]_remainingTime";
+	 my $EPICS_STATE = "pLocalEpics->$::systemName\.$::xpartName[$i]\_STATE";
+	 my $EPICS_BPTIME = "pLocalEpics->$::systemName\.$::xpartName[$i]\_BPTIME";
+	 my $EPICS_BPSET = "pLocalEpics->$::systemName\.$::xpartName[$i]\_BPSET";
+	 my $EPICS_RESET = "pLocalEpics->$::systemName\.$::xpartName[$i]\_RESET";
+
+        return <<END;
+
+// DACKILLER MODULE
+if ($EPICS_RESET) {
+$STATE = 1;
+$BPTIME_REMAINING = 0;
+$EPICS_RESET = 0;
+} 
+if ($BPTIME_REMAINING > 0) {
+$STATE = 1;
+$DACSTAT = 2;
+} else {
+$STATE = ($STATE & (int)$SIGNAL);
+$DACSTAT = $STATE;
+}
+if ($EPICS_BPSET) {
+$BPTIME_REMAINING = $BPTIME;
+$EPICS_BPSET = 0;
+} 
+if (!$CYCLE) {
+if($BPTIME_REMAINING) $BPTIME_REMAINING -= 1;
+$EPICS_STATE = $DACSTAT;
+$EPICS_BPTIME = $BPTIME_REMAINING;
+} 
+END
+}
+
