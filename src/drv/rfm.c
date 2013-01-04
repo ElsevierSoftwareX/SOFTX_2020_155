731d7c42bc196082c810f66628f076f00b995ac0
Rolf Bork <rolf.bork@ligo.org>
2013-01-04 18:05:33 +0000

    1) Removed old ifdefs for using VMIC5579 and VMIC5565 RFM for EPICS communications. 2) Added doxygen commentary.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@3165 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/drv/rfm.c
+++ src/drv/rfm.c
@@ -1,3 +1,6 @@
+///	@file rfm.c
+/// 	@brief Routines for finding shared memory locations.
+
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -13,144 +16,14 @@
 #include <signal.h>
 #include <ctype.h>
 
-#ifdef NO_RTL
 #include "mbuf/mbuf.h"
-#endif
 
-
-#ifdef SOLARIS
-#include <rfmApi.h>
-
-static char	*rfmFn = "/dev/rfm0";
-int rfm_size = 0;
-volatile unsigned char *rm_mem_ptr;
-static RFMHANDLE	rh;			/* Where RFM gets opened	 */
-
-unsigned int findRfmCard(unsigned int bn)
-{
-  static initFlag = 1;
-  if (initFlag) {
-    if ((rh = rfmOpen (rfmFn)) == 0) {
-      fprintf(stderr," can't open RFM device `%s'; errno=%d", rfmFn, errno);
-      exit (1);
-    }
-    /* Determine how much memory is associated with this device	 */
-    rfm_size = rfmSize(rh);
-
-    printf("%d=0x%x bytes of VMIC Reflected Memory in `%s'\n", rfm_size, rfm_size, rfmFn);
-    rm_mem_ptr = rfmRfm(rh)->rfm_ram;
-    if (! rm_mem_ptr) {
-      fprintf(stderr, "failed to map VMIC Reflected Memory into the address space");
-      exit(1);
-    }
-    initFlag = 0;
-  }
-  return (unsigned int) rm_mem_ptr;
-}
-
-#else 
-
-#ifdef RFM_5579
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include <asm/page.h>
-#include "rmn_api.h"
-#include "rmn_version.h"
-
-#define DEFAULT_DEVICE   "/dev/rmnet0"
-#define PROCFILE         "/proc/rmnet"
-
-static  RMNHANDLE Handle;
-int rfm_size;
-
-unsigned int findRfmCard(unsigned int bn)
-{
-    int  result;
-    static char *outbuffer;           /* Pointer mapped to RFM area          */
-    static initFlag = 1;
-
-    if (initFlag) {
-    /* Open the Reflective Memory device */
-    result = rmnOpen( DEFAULT_DEVICE, &Handle, 0 );
-    if( result != RMN_SUCCESS )
-    {
-        printf( "ERROR: rmnOpen() failed.\n" );
-        exit(1);
-    }
-
-#ifdef RFM5579_SWAP_ON_SIZE
-    {
-      RMN_UINT8 mode = SWAP_ON_SIZE;
-      rmnSetSwapMode(Handle, mode);
-    }
-#endif
-
-    result = rmnMapMemory(Handle, (void **)(&outbuffer), 0, (rfm_size = Handle->Config.MemorySize) / PAGE_SIZE);
-    if( result != RMN_SUCCESS )
-    {
-        printf( "ERROR: rmnMapMemory() failed.\n" );
-        exit(1);
-    }
-    printf("5579 RFM card base address=0x%x\n", outbuffer);
-    initFlag = 0;
-    }
-    return (unsigned int) outbuffer;
-}
-#elif defined(RFM_5565)
-
-#include <asm/page.h>
-#include "rfm2g_api.h"
-int rfm_size;
-
-#define DEVICE "/dev/rfm2g0"
-static  RFM2GHANDLE Handle;
-
-unsigned int findRfmCard(unsigned int bn)
-{
-    RFM2G_STATUS  result;              /* Return codes from RFM2Get API calls */
-    static RFM2G_UINT32 *outbuffer;           /* Pointer mapped to RFM area          */
-    static initFlag = 1;
-
-
-    if (initFlag) {
-    /* Open the Reflective Memory device */
-    result = RFM2gOpen( DEVICE, &Handle );
-    if( result != RFM2G_SUCCESS )
-    {
-        printf( "ERROR: RFM2gOpen() failed.\n" );
-        printf( "ERROR MSG: %s\n", RFM2gErrorMsg(result));
-        exit(1);
-    }
-
-    result = RFM2gUserMemory(Handle, (void **)(&outbuffer), 0, (rfm_size = Handle->Config.MemorySize) / PAGE_SIZE);
-    if( result != RFM2G_SUCCESS )
-    {
-        printf( "ERROR: RFM2gUserMemory() failed.\n" );
-        printf( "ERROR MSG: %s\n", RFM2gErrorMsg(result));
-        exit(1);
-    }
-    printf("5565 RFM card base address=0x%x\n", outbuffer);
-    initFlag = 0;
-    }
-    return (unsigned int) outbuffer;
-}
-#else
-
+/// Pointer to start of shared memory segment for a selected system.
 volatile unsigned char *addr = 0;
 
-
-
-/*
-  Search shared memory device file names in /rtl_mem_*
-  These are like /rtl_mem_das for DAS system,
-  /rtl_mem_pde for PDE system.
-*/
+///  Search shared memory device file names in /rtl_mem_*
+///	@param[in] *sys_name	Name of system, required to attach shared memory.
+///	@return			Pointer to start of shared memory segment for this system.
 volatile void *
 findSharedMemory(char *sys_name)
 {
@@ -163,22 +36,8 @@ findSharedMemory(char *sys_name)
 
 	sprintf(fname, "/rtl_mem_%s", sys);
 
-
-#ifdef NO_RTL
 	int ss = 64*1024*1024;
   	if (!strcmp(sys_name, "ipc")) ss = 4*1024*1024;
-#if 0
-       #include <sys/mman.h>
-       #include <sys/stat.h>        /* For mode constants */
-       #include <fcntl.h>           /* For O_* constants */
-       //int shm_open(const char *name, int oflag, mode_t mode);
-       if ((fd=shm_open(fname, O_RDWR | O_CREAT, 0777)) < 0) {
-                fprintf(stderr, "Couldn't shm_open `%s' read/write\n", fname);
-                return 0;
-
-       }
-       ftruncate(fd, ss);
-#endif
 
        if ((fd = open ("/dev/mbuf", O_RDWR | O_SYNC)) < 0) {
 		fprintf(stderr, "Couldn't open /dev/mbuf read/write\n");
@@ -189,14 +48,6 @@ findSharedMemory(char *sys_name)
        strcpy(req.name, sys);
        ioctl (fd, IOCTL_MBUF_ALLOCATE, &req);
        ioctl (fd, IOCTL_MBUF_INFO, &req);
-#else
-const int ss = 64*1024*1024-5000;
-        if ((fd=open(fname, O_RDWR))<0) {
-		fprintf(stderr, "Couldn't open `%s' read/write\n", fname);
-                return 0;
-        }
-#endif
-
 	
         addr = (volatile unsigned char *)mmap(0, ss, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
         if (addr == MAP_FAILED) {
@@ -205,11 +56,16 @@ const int ss = 64*1024*1024-5000;
                 _exit(-1);
         }
 	printf("mmapped address is 0x%lx\n", (long)addr);
-//	memset((void *)addr, 0, ss);
         return addr;
 }
 
-
+/// Find and return pointer to shared memory. This is old, from when EPICS memory was on
+/// VMIC RFM cards.
+/// This function should be deleted in favor just having findSharedMemory return pointer,
+/// but need to find/change all code that calls this function first.
+/// Note that this routine does nothing but return addr, which was found in findSharedMemory().
+///	@param[in] bn	RFM board number (0,1) - should now always be zero
+///	@return			Pointer to start of shared memory segment for this system.
 void *
 findRfmCard(unsigned int bn)
 {
@@ -231,5 +87,3 @@ findRfmCard(unsigned int bn)
     }
     return (void *)addr;
 }
-#endif
-#endif
