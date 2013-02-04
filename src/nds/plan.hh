79a4fd8e10e4b8071fd75aeac0782d46d342f762
alexander.ivanov@LIGO.ORG <alexander.ivanov@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2013-02-04 20:23:34 +0000

    Cleaned up fast reader code, moved some code into the constructor.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@3251 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/nds/plan.hh
+++ src/nds/plan.hh
@@ -12,7 +12,18 @@
 class plan : public FrameCPP::Common::IFrameStream
 {
 public:
-  plan( buffer_type* Stream, plan *master = 0 ) : IFrameStream(Stream), master_plan(master) {}
+  plan( buffer_type* Stream, plan *master = 0 ) : IFrameStream(Stream), master_plan(master) {
+	if (master_plan) {
+	  // Will not load the TOC but rather use the master plan TOC 
+	  // :TODO: need to see if the TOC size is still the same and
+	  // that the frame sequence is continuous
+	  // Assign the TOC pointer from the mater plan
+	  m_toc = master_plan->m_toc;
+	  m_toc_loaded = true;
+	  // Copy over the hash, needed to resolve structure types
+	  m_stream_id_to_fsi_id = master_plan->m_stream_id_to_fsi_id;
+	}
+  }
   ~plan() {}
 
 
@@ -27,33 +38,24 @@ public:
   }
 
   fr_adc_data_type ReadFrAdcData( INT_4U Frame, const std::string& Channel ) {
-      if (master_plan && !m_toc_loaded) {
-	// Will not load the TOC but rather use the master plan TOC 
-	// :TODO: need to see if the TOC size is still the same and
-	// that the frame sequence is continuous
-	// Assign the TOC pointer from the mater plan
-	m_toc = master_plan->m_toc;
-	// Copy over the hash, needed to resolve structure types
-	// :TODO: this needs to come out of here into the contructor
-	m_stream_id_to_fsi_id = master_plan->m_stream_id_to_fsi_id;
-	m_toc_loaded = true;
+      if (master_plan) {
 	// Populate index entry for the channel we are interested in
 	FrameCPP::Version::FrTOC *v8_toc =
 		dynamic_cast<FrameCPP::Version::FrTOC *>(m_toc.get());
 	FrameCPP::Version::FrTOCAdcData::MapADC_type& adcMap =
 		const_cast<FrameCPP::Version::FrTOCAdcData::MapADC_type&>(v8_toc->GetADC());
 	FrameCPP::Version::FrTOCAdcData::MapADC_type::iterator i = adcMap.find(Channel);
-	std::cerr << "adc seq num: " << i->second.m_index << std::endl;
-	std::cerr << "adc toc positions start: " << v8_toc->m_positions_start << std::endl;
+	//std::cerr << "adc seq num: " << i->second.m_index << std::endl;
+	//std::cerr << "adc toc positions start: " << v8_toc->m_positions_start << std::endl;
 
 	// read new ADC positions at v8_toc->m_positions_start + i->second.m_index * 8
         seekg(-v8_toc->m_positions_start, std::ios_base::end );
         seekg(i->second.m_index * 8, std::ios_base::cur );
-	std::cerr << "curent file positions is " << tellg( ) << std::endl;
+	//std::cerr << "curent file positions is " << tellg( ) << std::endl;
 
 	// assign new position
 	*this >> i->second.m_positionADC[ 0 ]; //i->second.m_positionADC[ 0 ];
-	std::cerr << "adc position: " << i->second.m_positionADC[ 0 ] << std::endl;
+	//std::cerr << "adc position: " << i->second.m_positionADC[ 0 ] << std::endl;
       } 
       return General::DynamicPointerCast< typename fr_adc_data_type::element_type > ( readFrAdcData( Frame, Channel ) );
   }
