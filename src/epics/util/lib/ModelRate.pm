5b32df176cb0c976c2a70835c143f02d3ef0e54a
rolf.bork@LIGO.ORG <rolf.bork@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2013-10-22 23:52:02 +0000

    New part which outputs the RT code rate for the model.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@3523 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/lib/ModelRate.pm
@@ -0,0 +1,61 @@
+package CDS::ModelRate;
+use Exporter;
+@ISA = ('Exporter');
+
+#//     \page stateWord StateWord.pm
+#//     Documentation for StateWord.pm
+#//
+#// \n
+
+sub partType {
+        return ModelRate;
+}
+
+
+# Print Epics communication structure into a header file
+# Current part number is passed as first argument
+sub printHeaderStruct {
+        my ($i) = @_;
+        ;
+}
+
+# Print Epics variable definitions
+# Current part number is passed as first argument
+sub printEpics {
+        my ($i) = @_;
+        ;
+}
+
+# Print variable declarations int front-end file
+# Current part number is passed as first argument
+sub printFrontEndVars  {
+        my ($i) = @_;
+}
+
+# Return front end initialization code
+# Argument 1 is the part number
+# Returns calculated code string
+sub frontEndInitCode {
+        my ($i) = @_;
+        return "";
+}
+
+# Figure out part input code
+# Argument 1 is the part number
+# Argument 2 is the input number
+# Returns calculated input code
+sub fromExp {
+        my ($i, $j) = @_;
+	return "FE_RATE"
+
+}
+
+# Return front end code
+# Argument 1 is the part number
+# Returns calculated code string
+
+sub frontEndCode {
+        return "";
+}
+
+
