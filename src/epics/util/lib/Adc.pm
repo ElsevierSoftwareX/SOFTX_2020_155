7a598408aa9d5509b633551ff8763c9166dc900e
Rolf Bork <rolf.bork@ligo.org>
2014-02-06 21:33:41 +0000

    Added ADC channel overflow counters to auto-gen MEDM screens.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@3606 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/Adc.pm
+++ src/epics/util/lib/Adc.pm
@@ -177,7 +177,7 @@ sub createAdcMedm
         print "creating file $medmDir\/$fname \n";
         open(OUTMEDM, ">$medmDir/$fname") || die "cannot open $medmDir/$fname for writing ";
 
-        my $xpos = 0; my $ypos = 0; my $width = 726; my $height = 470;
+        my $xpos = 0; my $ypos = 0; my $width = 900; my $height = 430;
         $medmdata = ("CDS::medmGen::medmGenFile") -> ($medmDir,$file,$width,$height);
 
 	# ************* Create Banner ******************************************************************************
@@ -185,17 +185,25 @@ sub createAdcMedm
         $height = 22;
         $medmdata .= ("CDS::medmGen::medmGenRectangle") -> ($xpos,$ypos,$width,$height,$ecolors{blue},"","","");
         # Add Display Name
-        $xpos = 300; $ypos = 4; $width = 120; $height = 15;        
+        $xpos = 370; $ypos = 4; $width = 120; $height = 15;        
 	$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,"$mdlName\_MONITOR_ADC$::adcCardNum[$adcNum]",$ecolors{white});
         # Add time string to banner
-        $xpos = 526; $ypos = 4; $width = 200; $height = 15;
+        $xpos = 640; $ypos = 4; $width = 200; $height = 15;
         $medmdata .= ("CDS::medmGen::medmGenTextMon") -> ($xpos,$ypos,$width,$height,"$site\:FEC-$dcuid\_TIME_STRING",$ecolors{white},$ecolors{blue},"static");
 
 
         # ************* Create Background **************************************************************************
         # Add Background rectangles
-        $xpos = 13; $ypos = 37; $width = 700; $height = 380;
+        $xpos = 13; $ypos = 37; $width = 870; $height = 380;
         $medmdata .= ("CDS::medmGen::medmGenRectangle") -> ($xpos,$ypos,$width,$height,$ecolors{gray},"","","");
+        $xpos = 300; $ypos = 38; $width = 60; $height = 15;
+	$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,"VALUE",$ecolors{black});
+	$xpos = 370;
+	$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,"OVERFLOW",$ecolors{black});
+        $xpos = 720;
+	$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,"VALUE",$ecolors{black});
+	$xpos = 790;
+	$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,"OVERFLOW",$ecolors{black});
 
         # ************* Add Text  **********************************************************************************
 	$xpos = 100; $ypos = 47; $width = 60; $height = 15;
@@ -210,22 +218,26 @@ sub createAdcMedm
 			$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,$ii,$ecolors{black});
 			$xpos = 300; $width = 60;
 			$medmdata .= ("CDS::medmGen::medmGenTextMon") -> ($xpos,$ypos,$width,$height,"$adcChannel[$adcNum][$ii]",$ecolors{white},$ecolors{blue},"static");
+			$xpos = 370;
+        		$medmdata .= ("CDS::medmGen::medmGenTextMon") -> ($xpos,$ypos,$width,$height,"$site\:FEC-$dcuid\_ADC_OVERFLOW_$adcNum\_$ii",$ecolors{white},$ecolors{blue},"static");
 		}
 		$ypos += 20;
 	}
 
-	$xpos = 300; $ypos = 47; $width = 60; $height = 15;
+	$xpos = 720; $ypos = 47; $width = 60; $height = 15;
 	for($ii=16;$ii<32;$ii++)
 	{
 		if(($ii % 4) == 0) {$ypos += 10;}
-		$xpos = 380; $width = 250;
+		$xpos = 460; $width = 250;
 		$labelName = substr $adcChannel[$adcNum][$ii],7;
 		$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,"$labelName",$ecolors{black});
 		if($labelName ne "") {
-			$xpos = 625; $width = 10;
+			$xpos = 705; $width = 10;
 			$medmdata .= ("CDS::medmGen::medmGenText") -> ($xpos,$ypos,$width,$height,$ii,$ecolors{black});
-			$xpos = 640; $width = 60;
+			$xpos = 720; $width = 60;
 			$medmdata .= ("CDS::medmGen::medmGenTextMon") -> ($xpos,$ypos,$width,$height,"$adcChannel[$adcNum][$ii]",$ecolors{white},$ecolors{blue},"static");
+			$xpos = 790;
+        		$medmdata .= ("CDS::medmGen::medmGenTextMon") -> ($xpos,$ypos,$width,$height,"$site\:FEC-$dcuid\_ADC_OVERFLOW_$adcNum\_$ii",$ecolors{white},$ecolors{blue},"static");
 		}
 		$ypos += 20;
 	}
