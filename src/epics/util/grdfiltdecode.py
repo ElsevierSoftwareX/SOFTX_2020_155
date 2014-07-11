4b9942b45dc7e7a6d80cfdd4dfebe85a89b9d203
Rolf Bork <rolf.bork@ligo.org>
2014-07-11 21:17:11 +0000

    Script to bring up FM displays from Guardian/Burt Restore screen.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@3711 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/epics/util/grdfiltdecode.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Automated test support classes
+
+import epics
+import time
+import string
+import sys, argparse
+import os
+
+from epics import PV
+from epics import caget,caput
+from subprocess import call
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-i',dest='chname')
+parser.add_argument('-m',dest='model')
+args = parser.parse_args()
+basedir = '/opt/rtcds/lho/h1/medm/h1fe3tim02/'
+
+ec = PV(args.chname)
+grdchan = ec.value
+
+mc = PV(args.model)
+word = mc.value.split("_")
+modelname = word[0].upper()
+
+if '_SW1S' in grdchan or '_SW2S' in grdchan:
+	x = len(grdchan) - 5
+	medmDisplay = ''
+	temp1 = grdchan[:x] + '.adl'
+	word = temp1.split('-')
+	for x in word:
+		print "Split ",x
+	temp2 = temp1.replace(":","")
+	temp3 = modelname + '_' + word[1]
+	medmDisplay = basedir + temp3
+	print 'Value of ',grdchan, ' is ', medmDisplay
+	print 'Model is ',modelname
+	call(["medm","-attach",medmDisplay])
+else:
+	print 'NOT A SWITCH CHANNEL'
