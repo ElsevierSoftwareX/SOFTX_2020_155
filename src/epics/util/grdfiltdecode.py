53816865bcf5229bb02a6236c20dc6c8d91a8d7f
Keith Thorne <keith.thorne@ligo.org>
2014-10-24 16:57:50 +0000

    added -x to medm call
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@3850 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/grdfiltdecode.py
+++ src/epics/util/grdfiltdecode.py
@@ -15,28 +15,30 @@ from subprocess import call
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-i',dest='chname')
 parser.add_argument('-m',dest='model')
+parser.add_argument('-d',dest='medmdir')
+parser.add_argument('-rcgd',dest='rcgdir')
 args = parser.parse_args()
-basedir = '/opt/rtcds/lho/h1/medm/h1fe3tim02/'
+basedir = args.medmdir
+filtalhdisplay = args.rcgdir + 'FILTALH.adl'
 
 ec = PV(args.chname)
 grdchan = ec.value
 
-mc = PV(args.model)
-word = mc.value.split("_")
-modelname = word[0].upper()
+modelname = args.model.upper()
+
 
 if '_SW1S' in grdchan or '_SW2S' in grdchan:
 	x = len(grdchan) - 5
 	medmDisplay = ''
+	temp = grdchan[:x]
+	ifo,filt = temp.split(':')
 	temp1 = grdchan[:x] + '.adl'
 	word = temp1.split('-')
-	for x in word:
-		print "Split ",x
 	temp2 = temp1.replace(":","")
 	temp3 = modelname + '_' + word[1]
 	medmDisplay = basedir + temp3
-	print 'Value of ',grdchan, ' is ', medmDisplay
-	print 'Model is ',modelname
-	call(["medm","-attach",medmDisplay])
+	myargs = '"FPREFIX=' + ifo +',FNAME=' + filt +',RDISP=' + medmDisplay
+	#call(["medm","-x","-attach","-macro",myargs,filtalhdisplay])
+	call(["medm","-attach","-x","-macro",myargs,filtalhdisplay])
 else:
 	print 'NOT A SWITCH CHANNEL'
