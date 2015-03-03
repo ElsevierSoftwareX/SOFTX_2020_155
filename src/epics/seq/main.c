333b912d692e248cc635297e923084e86db6e5b7
Rolf Bork <rolf.bork@ligo.org>
2015-03-03 17:57:05 +0000

    More SDF work: - Added _SWSTR EPICS record for each filter module. This provides a string representation   of filter module switch states. - Modified display of filter module switch settings in SDF Diff table to use new record. - Added some more error checking to SDF code.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/branches/branch-2.9@3969 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/seq/main.c
+++ src/epics/seq/main.c
@@ -199,14 +199,15 @@ char psd[6][64];
 				}
 				break;
 			case IS_A_QUOTE:
-				inQuotes ^= 1;
-				if(inQuotes == 0) lastwasspace = 1;
+				inQuotes += 1;
+				if(inQuotes == 2) lastwasspace = 1;
 				break;
 		}
 		s ++;
 	}
 	wc ++;
 	sprintf(str1,"%s",psd[0]);
+	if(inQuotes > 2) return(-1);
 	sprintf(str2,"%s",psd[1]);
 	sprintf(str3,"%s",psd[2]);
 	sprintf(str4,"%s",psd[3]);
@@ -250,6 +251,38 @@ unsigned int filtCtrlBitConvert(unsigned int v) {
         }
         return val;
 }
+/// Quick convert of filter switch SWSTAT to SWSTR value
+///	@param[in] v			SWSTAT representation of switch setting.	
+///	@param[out] filterstring	Returned string representation of switch setting.	
+///	@return				Always zero at this point.
+int filtStrBitConvert(int stype, unsigned int v, char *filterstring) {
+unsigned int val[5],jj;
+unsigned int x;
+char bitDefShort[16][8] = {"IN,","OF,","1,","2,",
+			   "3,","4,","5,","6,",
+			   "7,","8,","9,","10,",
+			   "LT,","OT,","DC,","HD,"};
+char bitDefLong[16][8] = {"IN,","OF,","F1,","F2,",
+			   "F3,","F4,","F5,","F6,",
+			   "F7,","F8,","F9,","F10,",
+			   "LT,","OT,","DC,","HD,"};
+
+	val[0] = (v >> 10) & 3;		//In and Offset
+	val[1] = (v & 1023) << 2;		// F1 thru F10
+	val[2] = (v & 0x2000) >> 1;		// Limit
+	val[3] = (v & 0x1000) << 1;		// Output
+	val[4] = (v & 0x18000) >> 1;		// Dec and Hold
+	x = val[0] + val[1] + val[2] + val[3] + val[4];
+	sprintf(filterstring,"%s","");
+	for(jj=0;jj<16;jj++) {
+		if(x & 1 && stype == 0) strcat(filterstring,bitDefShort[jj]);
+		if(x & 1 && stype == 1) strcat(filterstring,bitDefLong[jj]);
+		x = x >> 1;
+	}
+	filterstring[strlen(filterstring) - 1] = 0;
+	// strcat(filterstring,"\0");
+        return(0);
+}
 
 /// Routine to check filter switch settings and provide info on switches not set as requested.
 /// @param[in] fcount	Number of filters in the control model.
@@ -262,11 +295,13 @@ unsigned int presentVal;
 unsigned int x,y;
 int ii,jj;
 int errCnt = 0;
-char swname[2][64];
+char swname[3][64];
 char tmpname[64];
-char swstate[2][4] = {"OFF","ON"};
 time_t mtime;
 char localtimestring[256];
+char swstrE[64];
+char swstrB[64];
+char swstrD[64];
 struct buffer {
 	DBRstatus
 	DBRtime
@@ -275,80 +310,65 @@ struct buffer {
 long options = DBR_STATUS|DBR_TIME;
 dbAddr paddr;
 dbAddr paddr1;
+dbAddr paddr2;
 long nvals = 1;
 long status;
-// Define names for SWSTAT bits.
-char bitDef[17][12] = { " [FM1]"," [FM2]", " [FM3]", " [FM4]",
-			" [FM5]"," [FM6]", " [FM7]", " [FM8]",
-			" [FM9]", " [FM10]"," [INPUT]", " [OFFSET]",
-			" [OUTPUT]"," [LIMIT]"," [CLR]"," [DEC]",
-			" [HOLD]"};
-// Map SWSTAT bits as being part os SW1S or SW2S
-int sw2record[17] = {0,0,0,0,
-		    0,0,1,1,	
-		    1,1,0,0,	
-		    1,1,0,1,1};	
-
 	for(ii=0;ii<fcount;ii++)
 	{
-		if(filterTable[ii].init && (filterTable[ii].mask || monitorAll))
-		{
-			bzero(swname[0],strlen(swname[0]));
-			bzero(swname[1],strlen(swname[1]));
-			strcpy(swname[0],filterTable[ii].fname);
-			strcat(swname[0],"SW1S");
-			strcpy(swname[1],filterTable[ii].fname);
-			strcat(swname[1],"SW2S");
-			status = dbNameToAddr(swname[0],&paddr);
-			status = dbGetField(&paddr,DBR_LONG,&buffer[0],&options,&nvals,NULL);
-			status = dbNameToAddr(swname[1],&paddr1);
-			status = dbGetField(&paddr1,DBR_LONG,&buffer[1],&options,&nvals,NULL);
-			for(jj=0;jj<2;jj++) {
-				if(buffer[jj].rval > 0xffff || buffer[jj].rval < 0)	// Switch setting overrange
-				{
-					sprintf(setErrTable[errCnt].chname,"%s", swname[jj]);
-					sprintf(setErrTable[errCnt].burtset, "%s", " ");
-					sprintf(setErrTable[errCnt].liveset, "0x%x", buffer[jj].rval);
-					sprintf(setErrTable[errCnt].diff, "%s", "OVERRANGE");
-					setErrTable[errCnt].sigNum = filterTable[ii].sw[jj];
-					mtime = buffer[jj].time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH;
-					strcpy(localtimestring, ctime(&mtime));
-					localtimestring[strlen(localtimestring) - 1] = 0;
-					sprintf(setErrTable[errCnt].timeset, "%s", localtimestring);
-					buffer[jj].rval &= 0xffff;
-					errCnt ++;
-				}
-			}
-			presentVal = buffer[0].rval + (buffer[1].rval << 16);
-			refVal = filtCtrlBitConvert(presentVal);
-			if(refVal != filterTable[ii].swreq && errCnt < SDF_ERR_TSIZE)
+		bzero(swname[0],strlen(swname[0]));
+		bzero(swname[1],strlen(swname[1]));
+		strcpy(swname[0],filterTable[ii].fname);
+		strcat(swname[0],"SW1S");
+		strcpy(swname[1],filterTable[ii].fname);
+		strcat(swname[1],"SW2S");
+		sprintf(swname[2],"%s",filterTable[ii].fname);
+		strcat(swname[2],"SWSTR");
+		status = dbNameToAddr(swname[0],&paddr);
+		status = dbGetField(&paddr,DBR_LONG,&buffer[0],&options,&nvals,NULL);
+		status = dbNameToAddr(swname[1],&paddr1);
+		status = dbGetField(&paddr1,DBR_LONG,&buffer[1],&options,&nvals,NULL);
+		for(jj=0;jj<2;jj++) {
+			if(buffer[jj].rval > 0xffff || buffer[jj].rval < 0)	// Switch setting overrange
 			{
-				x = refVal;
-				y = filterTable[ii].swreq;
-				for(jj=0;jj<17;jj++)
-				{
-					if((x & 1) != (y & 1) && errCnt < SDF_ERR_TSIZE)
-					{
-						bzero(tmpname,sizeof(tmpname));
-						strncpy(tmpname,filterTable[ii].fname,(strlen(filterTable[ii].fname)-1));
-						strcat(tmpname,bitDef[jj]);
-
-						sprintf(setErrTable[errCnt].chname,"%s", tmpname);
-						sprintf(setErrTable[errCnt].burtset, "%s", swstate[(y&1)]);
-						sprintf(setErrTable[errCnt].liveset, "%s", swstate[(x&1)]);
-						setErrTable[errCnt].sigNum = filterTable[ii].sw[sw2record[jj]];
-
-						mtime = buffer[sw2record[jj]].time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH;
-						strcpy(localtimestring, ctime(&mtime));
-						localtimestring[strlen(localtimestring) - 1] = 0;
-						sprintf(setErrTable[errCnt].timeset, "%s", localtimestring);
-						sprintf(setErrTable[errCnt].diff, "%s", "               ");
-						errCnt ++;
-					}
-					x = x >> 1;
-					y = y >> 1;
-				}
+				sprintf(setErrTable[errCnt].chname,"%s", swname[jj]);
+				sprintf(setErrTable[errCnt].burtset, "%s", " ");
+				sprintf(setErrTable[errCnt].liveset, "0x%x", buffer[jj].rval);
+				sprintf(setErrTable[errCnt].diff, "%s", "OVERRANGE");
+				setErrTable[errCnt].sigNum = filterTable[ii].sw[jj];
+				mtime = buffer[jj].time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH;
+				strcpy(localtimestring, ctime(&mtime));
+				localtimestring[strlen(localtimestring) - 1] = 0;
+				sprintf(setErrTable[errCnt].timeset, "%s", localtimestring);
+				buffer[jj].rval &= 0xffff;
+				errCnt ++;
 			}
+		}
+		presentVal = buffer[0].rval + (buffer[1].rval << 16);
+		refVal = filtCtrlBitConvert(presentVal);
+		filtStrBitConvert(0,refVal,swstrE);
+		x = refVal;
+		status = dbNameToAddr(swname[2],&paddr2);
+		status = dbPutField(&paddr2,DBR_STRING,swstrE,1);
+		if(refVal != filterTable[ii].swreq && errCnt < SDF_ERR_TSIZE && (filterTable[ii].mask || monitorAll) && filterTable[ii].init)
+		{
+			filtStrBitConvert(1,refVal,swstrE);
+			filtStrBitConvert(1,filterTable[ii].swreq,swstrB);
+			x = refVal ^ filterTable[ii].swreq;;
+			filtStrBitConvert(1,x,swstrD);
+			bzero(tmpname,sizeof(tmpname));
+			strncpy(tmpname,filterTable[ii].fname,(strlen(filterTable[ii].fname)-1));
+
+			sprintf(setErrTable[errCnt].chname,"%s", tmpname);
+			sprintf(setErrTable[errCnt].burtset, "%s", swstrB);
+			sprintf(setErrTable[errCnt].liveset, "%s", swstrE);
+			sprintf(setErrTable[errCnt].diff, "%s", swstrD);
+			setErrTable[errCnt].sigNum = filterTable[ii].sw[0] + (filterTable[ii].sw[1] * SDF_MAX_TSIZE);
+
+			mtime = buffer[0].time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH;
+			strcpy(localtimestring, ctime(&mtime));
+			localtimestring[strlen(localtimestring) - 1] = 0;
+			sprintf(setErrTable[errCnt].timeset, "%s", localtimestring);
+			errCnt ++;
 		}
 	}
 	return(errCnt);
@@ -926,8 +946,16 @@ int resetSelectedValues(int errNum)
 long status;
 int ln = errNum - 1;
 int ii = setErrTable[ln].sigNum;
+int jj;
 dbAddr saddr;
 	
+	if(ii > SDF_MAX_TSIZE) {
+		jj = ii / SDF_MAX_TSIZE;
+		status = dbNameToAddr(cdTable[jj].chname,&saddr);
+		if(cdTable[jj].datatype == SDF_NUM) status = dbPutField(&saddr,DBR_DOUBLE,&cdTable[jj].data.chval,1);
+		else status = dbPutField(&saddr,DBR_STRING,&cdTable[jj].data.strval,1);
+	}
+	ii = ii % SDF_MAX_TSIZE;
 	status = dbNameToAddr(cdTable[ii].chname,&saddr);
 	if(cdTable[ii].datatype == SDF_NUM) status = dbPutField(&saddr,DBR_DOUBLE,&cdTable[ii].data.chval,1);
 	else status = dbPutField(&saddr,DBR_STRING,&cdTable[ii].data.strval,1);
@@ -1072,7 +1100,7 @@ int readConfig( char *pref,		///< EPICS channel prefix from EPICS environment.
 	char errMsg[128];
 	int argcount = 0;
 	int isalarm = 0;
-	int qcnt = 0;
+	int lineCnt = 0;
 
 	rderror = 0;
 
@@ -1089,7 +1117,7 @@ int readConfig( char *pref,		///< EPICS channel prefix from EPICS environment.
 		if(cdf == NULL) {
 			sprintf(errMsg,"New SDF request ERROR: FILE %s DOES NOT EXIST\n",sdfile);
 			logFileEntry(errMsg);
-			lderror = 2;
+			lderror = 4;
 			return(lderror);
 		}
 		chNumP = 0;
@@ -1101,8 +1129,18 @@ int readConfig( char *pref,		///< EPICS channel prefix from EPICS environment.
 		while(fgets(line,sizeof line,cdf) != NULL)
 		{
 			isalarm = 0;
+			lineCnt ++;
 			strcpy(s4,"x");
 			argcount = parseLine(line,s1,s2,s3,s4,s5,s6);
+			if(argcount == -1) {
+				sprintf(readErrTable[rderror].chname,"%s", s1);
+				sprintf(readErrTable[rderror].burtset, "%s", "Improper quotations ");
+				sprintf(readErrTable[rderror].liveset, "Line # %d", lineCnt);
+				sprintf(readErrTable[rderror].diff, "%s", sdfile);
+				sprintf(readErrTable[rderror].timeset, "%s", timestring);
+				rderror ++;
+				continue;
+			}
 			// Only 3 = no monit flag
 			// >=4 count be monit flag or string with quotes
 			// If 1st three chars match IFO ie checking this this line is not BURT header or channel marked RO
@@ -1170,11 +1208,8 @@ int readConfig( char *pref,		///< EPICS channel prefix from EPICS environment.
 							if(cdTable[ii].filterswitch) {
 								if(cdTableP[chNumP].data.chval > 0xffff) {
 									sprintf(readErrTable[rderror].chname,"%s", cdTable[ii].chname);
-
 									sprintf(readErrTable[rderror].burtset, "0x%x", (int)cdTableP[chNumP].data.chval);
-
 									sprintf(readErrTable[rderror].liveset, "%s", "OVERRANGE");
-
 									sprintf(readErrTable[rderror].diff, "%s", "MAX VAL = 0xffff");
 									sprintf(readErrTable[rderror].timeset, "%s", timestring);
 									rderror ++;
@@ -1217,7 +1252,7 @@ int readConfig( char *pref,		///< EPICS channel prefix from EPICS environment.
 					fclose(cdf);
 					sprintf(errMsg,"Number of channels in %s exceeds program limit\n",sdfile);
 					logFileEntry(errMsg);
-					lderror = 2;
+					lderror = 4;
 					return(lderror);
 				}
 		   	}
@@ -1244,6 +1279,7 @@ int readConfig( char *pref,		///< EPICS channel prefix from EPICS environment.
 	status = dbNameToAddr(reloadtimechannel,&paddr);
 	status = dbPutField(&paddr,DBR_STRING,timestring,1);
 	printf("Number of read errors = %d\n",rderror);
+	if(rderror) lderror = 2;
 	return(lderror);
 }
 
@@ -1550,19 +1586,12 @@ sleep(5);
 		// Check if file name != to one presently loaded
 		if(strcmp(sdf,loadedSdf) != 0) burtstatus |= 1;
 		else burtstatus &= ~(1);
-		// if(burtstatus & 1) status = dbPutField(&reloadtimeaddr,DBR_STRING,"New SDF File Pending",1);
-		switch(burtstatus) {
-			case 1:
+		if(burtstatus == 1)
 				status = dbPutField(&reloadtimeaddr,DBR_STRING,"New SDF File Pending",1);
-				break;
-			case 2:
-			case 3:
+		if(burtstatus & 2)
+				status = dbPutField(&reloadtimeaddr,DBR_STRING,"Read Error: Errant line(s) in file",1);
+		if(burtstatus & 4)
 				status = dbPutField(&reloadtimeaddr,DBR_STRING,"Read Error: File Not Found",1);
-				break;
-			default:
-				break;
-		}
-	
 		if(request != 0) {		// If there is a read file request, then:
 			status = dbPutField(&reload_addr,DBR_LONG,&ropts,1);	// Clear the read request.
 			reqValid = 1;
@@ -1570,7 +1599,7 @@ sleep(5);
 				rdstatus = readConfig(pref,sdfile,request);
 				if (rdstatus) burtstatus |= rdstatus;
 				else burtstatus &= ~(6);
-				if(burtstatus < 2) {
+				if(burtstatus < 4) {
 					switch (request){
 						case SDF_LOAD_DB_ONLY:
 							strcpy(loadedSdf,sdf); 
