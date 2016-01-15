cae7721290c20738dc858cd6f11fffc5ec986d59
Keith Thorne <keith.thorne@ligo.org>
2016-01-15 18:53:05 +0000

    add fstab
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4100 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ setup/gentoo3.2/fe/etc/load_dolphin_drivers.sh
@@ -0,0 +1,5 @@
+insmod /lib/modules/3.2.73/kernel/drivers/dis/dis_kosif.ko
+insmod /lib/modules/3.2.73/kernel/drivers/dis/dis_dx.ko dx_node_id=-1 dx_msi_enable=1 dx_pio_remote_interrupt=1 dx_dma_urgent_timer=0 
+dx_dma_normal_timer=0 dx_repl_enable=1 dx_binding_teardown=0 dx_srom_reset=0
+insmod /lib/modules/3.2.73/kernel/drivers/dis/dis_irm.ko pcisciInsmodParams=1 dis_max_segment_size_megabytes=4 sci_page_size=1024 
+sci_timeout_delay=0 max-vc-number=1024 dis_report_resource_outtages=1 linkWatchdogEnabled=10 enable_session_probes_while_heartbeat_ok=0 use128BytesSciPackets=1 enableStreamErrorStatistics=1 prefetch_space_map_size=64 numberNonPrefEntries=512 numberPrefEntries=7128 number-of-megabytes-preallocated=160 minimum-size-to-allocate-from-preallocated-pool=0 try-first-to-allocate-from-preallocated-pool=1 block-size-of-preallocated-blocks=0 number-of-preallocated-blocks=0 use-sub-pools-for-preallocation=1 dis_use_iommu=0 
