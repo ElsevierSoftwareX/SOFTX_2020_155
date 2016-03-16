bd6ecf9c564900993254e8267c0b176ddaabc3e0
rolf.bork@LIGO.ORG <rolf.bork@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2016-03-16 20:08:38 +0000

    Bug fix: If DIVIDE has more than 2 inputs, RCG would not recognize this and therefore produce a bad output to the C file.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4152 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/lib/DIVIDE.pm
+++ src/epics/util/lib/DIVIDE.pm
@@ -35,8 +35,10 @@ sub printFrontEndVars  {
 # Check inputs are connected
 sub checkInputConnect {
         my ($i) = @_;
-	if($::partInCnt[$i] < 2) {
-                print ::CONN_ERRORS "***\n$::partType[$i] with name $::xpartName[$i] has missing inputs\nRequires 2; Only $::partInCnt[$i] provided.\n";
+	# Use the length of the calculation string to determine number of inputs
+	my $nc = length($::partInputs[$i]);
+	if($::partInCnt[$i] != $nc) {
+                print ::CONN_ERRORS "***\n$::partType[$i] with name $::xpartName[$i] has missing inputs\nRequires $nc; Only $::partInCnt[$i] provided.\n";
 	        return "ERROR";
         }
         return "";
@@ -67,22 +69,66 @@ sub fromExp {
 sub frontEndCode {
 	my ($i) = @_;
 	my $from, $to;
-	if ($::partInputs[$i] eq "*/")  {
-         	$from = 0;
-	        $to = 1;
-	} else {
-         	$from = 1;
-	        $to = 0;
+	my $numIn = length($::partInputs[$i]);
+
+	my @chars = split('',$::partInputs[$i]);
+	my $jj = 0;
+	my $ii;
+	my $ptr = 0;
+	my @fn;
+	# Find the multiplies first
+	foreach $ii (@chars) {
+		if($ii eq "*") {
+			if($ptr > 0) {$operator[($ptr - 1)] = "*"; }
+			$fn[$ptr] = $jj;
+			$ptr ++;
+		}
+		$jj ++;
 	}
-        my $calcExp = <<HERE
-// DIVIDE
-\L$::xpartName[$i]\E = $::fromExp[$from] /
-        (($::fromExp[$to] < 0.0)
-                ?
-                (($::fromExp[$to] > -1e-20)? -1e-20: $::fromExp[$to])
-                :
-                (($::fromExp[$to] < 1e-20)? 1e-20: $::fromExp[$to]));
-HERE
-	;
+	$jj = 0;
+	# Find the divides
+	foreach $ii (@chars) {
+		if($ii eq "/") {
+			if($ptr > 0) {$operator[($ptr - 1)] = "/"; }
+			$fn[$ptr] = $jj;
+			$ptr ++;
+		}
+		$jj ++;
+	}
+	my $math = "";
+	my $nq = 0;
+	my @quals;
+	my $calcExp = "//DIVIDE \n";
+	for($ii=0;$ii<$ptr;$ii++) {
+		$math .= $::fromExp[$fn[$ii]] ;
+		$math .= " ";
+		$math .= $operator[$ii] ;
+		$math .= " ";
+		# If we have any divides, will need to verify the specified variables are not zero.
+		# If any are zero, we will not do calc and output 0.0.
+		if($operator[$ii] eq "/") {
+			if($nq == 0) {
+				$calcExp .= "if( ";
+				$calcExp .= $::fromExp[$fn[($ii+1)]]
+			}
+			if($nq > 0) {
+				$calcExp .= " && ";
+				$calcExp .= $::fromExp[$fn[($ii+1)]]
+			}
+			$calcExp .= " != 0.0 ";
+			$nq ++;
+		}
+	}
+	if($nq > 0) {
+		$calcExp .= ") {\n   ";
+	}
+	$calcExp .= "\L$::xpartName[$i]\E = ";
+	$calcExp .= $math;
+	$calcExp .= ";\n";
+	if($nq > 0) {
+		$calcExp .= "} else { \n   ";
+		$calcExp .= "\L$::xpartName[$i]\E = 0.0; \n} \n";
+	}
+
         return $calcExp;
 }
