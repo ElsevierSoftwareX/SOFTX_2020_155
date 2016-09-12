d9cd5d1e50b42a15b6c5e925a63424f34b1985ea
Rolf Bork <rolf.bork@ligo.org>
2016-09-12 17:22:21 +0000

    Updated version number for release.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/tags/advLigoRTS-3.1.1@4260 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/include/rcgversion.h
+++ src/include/rcgversion.h
@@ -1,4 +1,4 @@
 #define RCG_VERSION_MAJOR       3
 #define RCG_VERSION_MINOR       1
-#define RCG_VERSION_SUB	       	0
-#define RCG_VERSION_REL	       	0
+#define RCG_VERSION_SUB	       	1
+#define RCG_VERSION_REL	       	1
