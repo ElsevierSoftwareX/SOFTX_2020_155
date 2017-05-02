2ae619e13120d9a43bcd24e649d6b02096ca04a0
rolf.bork@LIGO.ORG <rolf.bork@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2017-05-02 20:02:08 +0000

    Some cleanup of 0MQ test software.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4333 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/zmq_stream/zmq_multi_rcvr.c
+++ src/zmq_stream/zmq_multi_rcvr.c
@@ -1,7 +1,6 @@
 //
-///	@file mx_stream.c
-///	@brief  Open-MX data sender, supports sending data
-///< from the IOP as well as from the slaves
+///	@file zmq_multi_rcvr.c
+///	@brief  Test DAQ data receiver using ZeroMQ.
 //
 
 #include <unistd.h>
@@ -26,57 +25,20 @@
 
 
 int do_verbose;
-volatile int threads_running;
 unsigned int do_wait = 0; // Wait for this number of milliseconds before starting a cycle
-unsigned int wait_delay = 4; // Wait before acknowledging sends with mx_wait() for this number of cycles times nsys
 
 extern void *findSharedMemory(char *);
 
 void
 usage()
 {
-	fprintf(stderr, "Usage: mx_stream [args] -s sys_names -d rem_host:0\n");
+	fprintf(stderr, "Usage: zmq_multi_rcvr [args] -s server name\n");
 	fprintf(stderr, "-l filename - log file name\n"); 
-	fprintf(stderr, "-s - server names \"x1x12 x1lsc x1asc\"\n");
-	fprintf(stderr, "-v - verbose\n");
-	fprintf(stderr, "-w - wait a given number of milliseconds before starting a cycle\n");
+	fprintf(stderr, "-s - server name eg x1lsc0, x1susex, etc.\n");
+	fprintf(stderr, "-v - verbose prints cpu_meter test data\n");
 	fprintf(stderr, "-h - help\n");
 }
 
-typedef struct blockProp {
-  unsigned int status;
-  unsigned int timeSec;
-  unsigned int timeNSec;
-  unsigned int run;
-  unsigned int cycle;
-  unsigned int crc; /* block data CRC checksum */
-} blockPropT;
-
-struct rmIpcStr {       /* IPC area structure                   */
-  unsigned int cycle;  /* Copy of latest cycle num from blocks */
-  unsigned int dcuId;          /* id of unit, unique within each site  */
-  unsigned int crc;            /* Configuration file's checksum        */
-  unsigned int command;        /* Allows DAQSC to command unit.        */
-  unsigned int cmdAck;         /* Allows unit to acknowledge DAQS cmd. */
-  unsigned int request;        /* DCU request of controller            */
-  unsigned int reqAck;         /* controller acknowledge of DCU req.   */
-  unsigned int status;         /* Status is set by the controller.     */
-  unsigned int channelCount;   /* Number of data channels in a DCU     */
-  unsigned int dataBlockSize; /* Num bytes actually written by DCU within a 1/16 data block */
-  blockPropT bp [DAQ_NUM_DATA_BLOCKS];  /* An array of block property structures */
-};
-
-/* GDS test point table structure for FE to frame builder communication */
-typedef struct cdsDaqNetGdsTpNum {
-   int count; /* test points count */
-   int tpNum[DAQ_GDS_MAX_TP_NUM];
-} cdsDaqNetGdsTpNum;
-
-
-struct daq0mqdata {
-	daq_msg_header_t zmqheader;
-	char zmqDataBlock[DAQ_DCU_BLOCK_SIZE];
-};
 
 unsigned int nsys; // The number of mapped shared memories (number of data sources)
 static const int buf_size = DAQ_DCU_BLOCK_SIZE * 2;
@@ -149,6 +111,7 @@ main(int argc, char **argv)
 	assert (rc == 0);
 
 
+	// Receive DAQ data in an infinite loop ***********************************
 
 	do {
 		// Initialize 0MQ message buffer
@@ -156,27 +119,39 @@ main(int argc, char **argv)
 		// Get data when message size > 0
 		size = zmq_msg_recv(&message,daq_subscriber,0);
 		assert(size >= 0);
+		// Get pointer to message data
 		char *string = (char *)zmq_msg_data(&message);
-		// Copy data out of message buffer to local memory buffer
+		// Copy data out of 0MQ message buffer to local memory buffer
 		memcpy(daqbuffer,string,size);
 		// Destroy the received message buffer
 		zmq_msg_close(&message);
+		// *******************************************************************
 		// Following is test finding cpu meter data
 		// Set data pointer to start of received data block
-		dataPtr = (char *)&mxDataBlock.zmqDataBlock[0];;
-		for(ii=0;ii<mxDataBlock.dcuTotalModels;ii++) {
-			// Increment data pointer to start of next FE data block
-			if(ii>0) dataPtr += mxDataBlock.zmqheader[ii-1].dataBlockSize;
-			// Extract the cpu meter data for each FE
-			cpu_meter = (int *)dataPtr;
-			cpu_meter += 2;
-			mycpu[ii] = *cpu_meter;
+		if(do_verbose) {
+			dataPtr = (char *)&mxDataBlock.zmqDataBlock[0];;
+			for(ii=0;ii<mxDataBlock.dcuTotalModels;ii++) {
+				// Increment data pointer to start of next FE data block
+				if(ii>0) dataPtr += mxDataBlock.zmqheader[ii-1].dataBlockSize;
+				// Extract the cpu meter data for each FE
+				cpu_meter = (int *)dataPtr;
+				cpu_meter += 2;
+				mycpu[ii] = *cpu_meter;
+			}
+			// Print the CPU METER info on each 1 second mark
+			if(mxDataBlock.zmqheader[0].cycle == 0) {
+				printf("\n*******************************************************\n");
+				printf("Total DCU = %d\n", mxDataBlock.dcuTotalModels);
+				for(ii=0;ii<mxDataBlock.dcuTotalModels;ii++) {
+					printf("DCU = %d\tCPU METER = \t%d\n",mxDataBlock.zmqheader[ii].dcuId,mycpu[ii]);
+					printf("\tTime = %d %d\tSize = \t%d bytes\n",
+						mxDataBlock.zmqheader[ii].timeSec,
+						mxDataBlock.zmqheader[ii].timeNSec,
+						mxDataBlock.zmqheader[ii].dataBlockSize);
+				}
+			}
 		}
-		// Print the CPU METER info on each 1 second mark
-		if(mxDataBlock.zmqheader[0].cycle == 0) {
-			printf("Total DCU = %d\n", mxDataBlock.dcuTotalModels);
-			printf("CPU = \t%d\t%d\t%d\t%d \n",mycpu[0],mycpu[1],mycpu[2],mycpu[3]);
-		}
+		// *******************************************************************
 
 	}while(!myErrorSignal);
 
