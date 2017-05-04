3eb6e0139f6c200cc23a23c97aeba662b19b155a
jonathan.hanks@LIGO.ORG <jonathan.hanks@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2017-05-04 22:39:24 +0000

    Adding in an updated symmetricom driver.
    This driver builds cleanly on linux 4.4.13.
    It probably works with 2.6.x but testing is needed.
    
    
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4335 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/drv/symmetricom-4.4/stest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "symmetricom.h"
+
+/* this is a test program that opens the mbuf_drv.
+   It reads out values of the kmalloc() and vmalloc()
+   allocated areas and checks for correctness.
+
+   Please install driver with "sudo make install" first.
+*/
+
+int main (void)
+{
+  int fd;
+
+  if ((fd = open ("/dev/symmetricom", O_RDWR | O_SYNC)) < 0) {
+      perror ("open");
+      exit (-1);
+  }
+
+  unsigned long req = 0;
+  unsigned long t[3];
+  ioctl (fd, IOCTL_SYMMETRICOM_STATUS, &req);
+  printf("%ld\n", req);
+  ioctl (fd, IOCTL_SYMMETRICOM_TIME, &t);
+  printf("%ds %du %dn\n", t[0], t[1], t[2]);
+  return(0);
+}
