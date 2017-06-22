f93c822fdddefab1f0458e46e8ffd862c39230df
jonathan.hanks@LIGO.ORG <jonathan.hanks@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2017-06-22 16:53:18 +0000

    Making a library version of the run_number client functionality.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4377 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/run_number/run_number_client.hh
@@ -0,0 +1,12 @@
+#ifndef DAQD_RUN_NUMBER_CLIENT_HH
+#define DAQD_RUN_NUMBER_CLIENT_HH
+
+#include <string>
+
+namespace daqd_run_number {
+
+    int get_run_number(const std::string& target, const std::string& hash);
+
+}
+
+#endif // DAQD_RUN_NUMBER_CLIENT_HH
