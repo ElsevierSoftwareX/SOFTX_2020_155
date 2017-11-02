38965deebbd58cee54e77f4700cdacc80c0e09ba
Jonathan Hanks <jonathan.hanks@ligo.org>
2017-11-02 23:44:46 +0000

    Updates to the simulated FE program.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4543 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/fe_stream_test/fe_stream_generator.hh
+++ src/fe_stream_test/fe_stream_generator.hh
@@ -11,71 +11,147 @@
 #include <sstream>
 #include <string>
 
+class SimChannel
+{
+private:
+    std::string name_;
+    int chtype_;
+    int rate_;
+    int chnum_;
+public:
+    SimChannel(): name_(""), chtype_(0), rate_(0), chnum_(0) {}
+    SimChannel(std::string name, int chtype, int rate, int chnum):
+            name_(name), chtype_(chtype), rate_(rate), chnum_(chnum) {}
+    SimChannel(const SimChannel& other):
+            name_(other.name_), chtype_(other.chtype_), rate_(other.rate_),
+            chnum_(other.chnum_) {}
+
+    SimChannel& operator=(const SimChannel& other)
+    {
+        if (this != &other)
+        {
+            name_ = other.name_;
+            chtype_ = other.chtype_;
+            rate_ = other.rate_;
+            chnum_ = other.chnum_;
+        }
+        return *this;
+    }
+
+    int data_type() const { return chtype_; }
+    int data_rate() const { return rate_; }
+    int channel_num() const { return chnum_; }
+
+    const std::string& name() const { return name_; }
+};
+
 class Generator
 {
 protected:
-    virtual int data_type() = 0;
-    virtual int data_rate() = 0;
-    virtual const std::string& channel_base_name() = 0;
+    virtual int data_type() const = 0;
+    virtual int data_rate() const = 0;
+    virtual const std::string& channel_base_name() const = 0;
 
-    virtual std::string other_params() { return ""; }
+    virtual std::string other_params() const { return ""; }
 public:
     virtual std::string generator_name() const = 0;
     virtual std::string full_channel_name() const
     {
         std::ostringstream os;
-        os << channel_base_name() << "--" << generator_name() << "--" << data_type() << "--" << data_rate() << other_params();
+        os << channel_base_name() << "--" << generator_name() << other_params() << "--" << data_type() << "--" << data_rate();
         return os.str();
     }
 
-    virtual template <typename output_iterator>
-    void generate(int gps_sec, int gps_nano, output_iterator out) = 0;
+    virtual char* generate(int gps_sec, int gps_nano, char *out) = 0;
+
+    virtual void output_ini_entry(std::ostream& os) = 0;
+    virtual void output_par_entry(std::ostream& os) = 0;
 };
 
 typedef std::tr1::shared_ptr<Generator> GeneratorPtr;
 
 namespace Generators {
-    class GeneratorParams: public Generator {
+    class SimChannelGenerator : public Generator {
     protected:
-        int data_type_;
-        int data_rate_;
-        std::string base_name_;
+        SimChannel ch_;
 
-        virtual int data_type() { return data_type_; };
-        virtual int data_rate() { return data_rate; };
-        virtual const std::string& channel_base_name() { return base_name_; };
+        virtual int data_type() const { return ch_.data_type(); };
+        virtual int data_rate() const { return ch_.data_rate(); };
+        virtual const std::string& channel_base_name() const { return ch_.name(); };
     public:
-        GeneratorParams(const std::string base_name, int data_type, int data_rate):
-                data_type_(data_type), data_rate_(data_rate), base_name_(base_name) {}
+        SimChannelGenerator(const SimChannel& ch):
+                ch_(ch) {}
+
+        virtual void output_ini_entry(std::ostream& os)
+        {
+            os << "[" << full_channel_name() << "]\n";
+            os << "datarate=" << data_rate() << "\n";
+            os << "datatype=" << data_type() << "\n";
+            os << "chnnum=" << ch_.channel_num() << "\n";
+        }
+
+        virtual void output_par_entry(std::ostream& os) {}
     };
 
-    class GPSSecondGenerator: public GeneratorParams
+    
+    
+    class GPSSecondGenerator: public SimChannelGenerator
     {
     public:
-        GPSSecondGenerator(const std::string base_name, int data_type, int data_rate):
-        GeneratorParams(base_name, data_type, data_rate) {}
+        GPSSecondGenerator(const SimChannel& ch):
+                SimChannelGenerator(ch) {}
 
         std::string generator_name() const { return "gps_sec"; }
 
-        virtual template <typename output_iterator>
-        void generate(int gps_sec, int gps_nano, output_iterator out)
+        char* generate(int gps_sec, int gps_nano, char* out)
         {
             int rate = data_rate() / 16;
+            int *out_ = reinterpret_cast<int*>(out);
             for (int i = 0; i < rate; ++i)
             {
-                *out = gps_sec;
-                ++out;
+                *out_ = gps_sec;
+                ++out_;
             }
+            return reinterpret_cast<char*>(out_);
+        }
+    };
+    
+    class GPSSecondWithOffset: public SimChannelGenerator
+    {
+        int offset_;
+    public:
+        GPSSecondWithOffset(const SimChannel& ch, int offset):
+                SimChannelGenerator(ch), offset_(offset) {}
+
+        std::string generator_name() const { return "gps_sec_off1p"; }
+
+        std::string other_params() const
+        {
+            std::ostringstream os;
+            os << "--" << offset_;
+            return os.str();
+        }
+
+        char* generate(int gps_sec, int gps_nano, char* out)
+        {
+            int rate = data_rate() / 16;
+            int *out_ = reinterpret_cast<int*>(out);
+            for (int i = 0; i < rate; ++i)
+            {
+                *out_ = gps_sec + offset_;
+                ++out_;
+            }
+            return reinterpret_cast<char*>(out_);
         }
     };
 }
 
-GeneratorPtr create_generator(const std::string generator, std::string& base_name, int data_type, int data_rate)
+GeneratorPtr create_generator(const std::string& generator, const SimChannel& ch)
 {
-    if (data_type != 4)
+    if (ch.data_type() != 4)
         throw std::runtime_error("Invalid/unsupported data type for a generator");
     if (generator == "gps_sec") {
-        return GeneratorPtr(new Generators::GPSSecondGenerator(base_name, date_type, data_rate));
+        return GeneratorPtr(new Generators::GPSSecondGenerator(ch));
     }
     throw std::runtime_error("Unknown generator type");
 }
--- src/fe_stream_test/fe_stream_test.cc
+++ src/fe_stream_test/fe_stream_test.cc
@@ -2,21 +2,31 @@
 // Simulate a FE model by producing known output and placing it in the appropriate shared memory block
 //
 
-#include <cstdlib>
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 
+#include <strings.h>
 #include <unistd.h>
 
+#include "../include/daqmap.h"
+#include "../zmq_stream/zmq_daq_core.h"
+
 #include "fe_stream_generator.hh"
+
+#include "gps.hh"
 
 extern "C" {
 
 extern volatile void* findSharedMemory(char* sys_name);
 
+#include "../drv/crc.c"
+
 }
+
 
 class ChNumDb
 {
@@ -28,57 +38,92 @@ public:
     int next(int channel_type) { max_++; return max_; }
 };
 
-class SimChannel
+std::string generate_ini_filename(const std::string &ini_dir, const std::string &system_name)
 {
-private:
-    std::string name_;
-    int chtype_;
-    int rate_;
-    int chnum_;
-public:
-    SimChannel(): name_(""), chtype_(0), rate_(0), chnum_(0) {}
-    SimChannel(std::string name, int chtype, int rate, int chnum):
-            name_(name), chtype_(0), rate_(rate), chnum_(chnum) {}
-    SimChannel(const SimChannel& other):
-            name_(other.name_), chtype_(other.chtype_), rate_(other.rate_),
-            chnum_(other.chnum_) {}
+    std::ostringstream ss;
+    ss << ini_dir << "/" << system_name << ".ini";
+    return ss.str();
+}
 
-    SimChannel& operator=(const SimChannel& other)
+std::string generate_par_filename(const std::string &ini_dir, const std::string &system_name)
+{
+    std::ostringstream ss;
+    ss << ini_dir << "/" << system_name << ".par";
+    return ss.str();
+}
+
+void output_ini_files(const std::string& ini_dir, const std::string& system_name, std::vector<GeneratorPtr> channels, int dcuid)
+{
+    using namespace std;
+
+    string fname_ini = generate_ini_filename(ini_dir, system_name);
+    string fname_par = generate_par_filename(ini_dir, system_name);
+    ofstream os_ini(fname_ini);
+    ofstream os_par(fname_par);
+    os_ini << "[default]\ngain=1.0\nacquire=1\ndcuid=" << dcuid << "\nifoid=0\n";
+    os_ini << "datatype=4\ndatarate=2048\noffset=0\nslope=1.0\nunits=undef\n\n";
+
+    vector<GeneratorPtr>::iterator cur = channels.begin();
+    for(; cur != channels.end(); ++cur)
     {
-        if (this != &other)
-        {
-            name_ = other.name_;
-            chtype_ = other.chtype_;
-            rate_ = other.rate_;
-            chnum_ = other.chnum_;
-        }
-        return *this;
+        Generator* gen = (cur->get());
+        (*cur)->output_ini_entry(os_ini);
     }
+}
 
-    const std::string& name() const { return name_; }
-};
+unsigned int calculate_ini_crc(const std::string& ini_dir, const std::string& system_name)
+{
+    std::string fname_ini = generate_ini_filename(ini_dir, system_name);
+    std::ifstream is(fname_ini.c_str(), std::ios::binary);
+
+    std::vector<char> buffer(64*1024);
+
+    size_t file_size = 0;
+    unsigned int file_crc = 0;
+    while (is.read(&buffer[0], buffer.size()))
+    {
+        file_crc = crc_ptr(&buffer[0], buffer.size(), file_crc);
+        file_size += buffer.size();
+    }
+    if (is.gcount() > 0)
+    {
+        file_crc = crc_ptr(&buffer[0], is.gcount(), file_crc);
+        file_size += is.gcount();
+    }
+    file_crc = crc_len(file_size, file_crc);
+    return file_crc;
+
+}
+
 
 void usage()
 {
     using namespace std;
     cout << "Usage: fe_stream_test" << endl;
+    cout << "-d dcuid" << endl;
     cout << "-s system name" << endl;
     cout << "-r data rate in MB/s of the simulation [1..256)" << endl;
     cout << "-i directory to put the ini and test point.par files in" << endl;
+    cout << "-S allow the use of the system clock" << endl;
 }
 
 int main(int argc, char *argv[]) {
     std::string system_name;
     std::string ini_dir;
     size_t data_rate=1;
+    int dcuid;
     bool verbose = false;
+    bool system_clock_ok = false;
 
     {
         int c;
-        while ((c = getopt(argc, argv, "hs:r:i:v")) != -1)
+        while ((c = getopt(argc, argv, "d:hs:r:i:vS")) != -1)
         {
             switch (c)
             {
+                case 'd':
+                    dcuid = std::atoi(optarg);
+                    break;
                 case 's':
                     system_name = optarg;
                     break;
@@ -90,6 +135,9 @@ int main(int argc, char *argv[]) {
                     break;
                 case 'v':
                     verbose = true;
+                    break;
+                case 'S':
+                    system_clock_ok = true;
                     break;
                 default:
                     usage();
@@ -107,13 +155,12 @@ int main(int argc, char *argv[]) {
         }
     }
     size_t req_size_bytes = data_rate * 1024*1024;
-    size_t channel_count = req_size_bytes / (4*8*1024);
+    size_t channel_count = req_size_bytes / (4*1024);
+    size_t block_size = (channel_count*4*1024)/16;
     if (verbose)
         std::cout << "Will create " << channel_count << " channels." << std::endl;
 
-    std::vector<SimChannel> channels;
     std::vector<GeneratorPtr> generators;
-    channels.reserve(channel_count);
     generators.reserve(channel_count);
 
     ChNumDb chDb;
@@ -124,17 +171,110 @@ int main(int argc, char *argv[]) {
 
         std::ostringstream ss;
         ss << system_name << "-CHANNEL_" << i;
-        channels.push_back(SimChannel(ss.str(), 4, 8*1024, chnum));
-        generators.push_back(create_generator("gps_sec", channels.back().name(), 4, 8*1024));
+        generators.push_back(GeneratorPtr(new Generators::GPSSecondWithOffset(SimChannel(ss.str(), 4, 1024, chnum), i%21)));
     }
 
     if (true)
     {
         std::cout << "Channels" << std::endl;
-        for (int i = 0; i < channels.size(); ++i)
+        for (int i = 0; i < generators.size(); ++i)
         {
-            std::cout << "\t" << channels[i].name() << std::endl;
+            std::cout << "\t" << generators[i]->full_channel_name() << std::endl;
         }
+    }
+
+    volatile char *shmem = 0;
+    {
+        std::ostringstream os;
+        os << system_name << "_daq";
+
+        shmem = reinterpret_cast<volatile char*>(findSharedMemory(const_cast<char*>(os.str().c_str())));
+        if (shmem == 0)
+            throw std::runtime_error("Received null shmem pointer");
+    }
+    volatile struct rmIpcStr* ipc = reinterpret_cast<volatile struct rmIpcStr*>(shmem + CDS_DAQ_NET_IPC_OFFSET);
+    volatile char *data = shmem + CDS_DAQ_NET_DATA_OFFSET;
+    volatile struct cdsDaqNetGdsTpNum* tpData = reinterpret_cast<volatile struct cdsDaqNetGdsTpNum*>(shmem + CDS_DAQ_NET_GDS_TP_TABLE_OFFSET);
+
+    output_ini_files(ini_dir, system_name, generators, dcuid);
+    unsigned int ini_crc = calculate_ini_crc(ini_dir, system_name);
+
+    GPS::gps_clock clock(0);
+    if (!clock.ok())
+    {
+        if (system_clock_ok)
+            std::cout << "Using system clock not Symmetricom driver" << std::endl;
+        else
+            throw std::runtime_error("Clock not synced to GPS time.");
+    }
+
+    bool buffer_ready = false;
+
+    ipc->cycle = -1;
+    ipc->dcuId = dcuid;
+    ipc->crc = ini_crc;
+    ipc->command = 0;
+    ipc->cmdAck = 0;
+    ipc->request = 0;
+    ipc->reqAck = 0;
+    ipc->status = 0xbad;
+    ipc->channelCount = generators.size();
+    ipc->dataBlockSize = block_size;
+    for (int i = 0; i < DAQ_NUM_DATA_BLOCKS; ++i)
+    {
+        ipc->bp[i].status = 0xbad;
+        ipc->bp[i].timeSec = 0;
+        ipc->bp[i].timeNSec = 0;
+        ipc->bp[i].run = 0;
+        ipc->bp[i].cycle = 0;
+        ipc->bp[i].crc = block_size;
+    }
+
+    GPS::gps_time time_step = GPS::gps_time(0, 1000000000/16);
+    GPS::gps_time transmit_time = clock.now();
+    ++transmit_time.sec;
+    transmit_time.nanosec = 0;
+
+    int cycle = 0;
+
+    while (true)
+    {
+        // The simulation writes 1/16s behind real time
+        // So we wait until the cycle start, then compute
+        // then write and wait for the next cycle.
+        GPS::gps_time now = clock.now();
+        while (now < transmit_time)
+        {
+            usleep(1);
+            now = clock.now();
+        }
+
+        if (verbose && cycle == 0)
+        {
+            std::cout << ". " << transmit_time << " " << now << std::endl;
+        }
+
+        volatile char *data_cur = data + cycle*block_size;
+        volatile char *data_end = data_cur + block_size;
+
+        for (int i = 0; i < generators.size(); ++i)
+        {
+            data_cur = generators[i]->generate(transmit_time.sec, transmit_time.nanosec, (char*)data_cur);
+            if (data_cur > data_end)
+                throw std::range_error("Generator exceeded its output boundary");
+        }
+
+        ipc->status = 0;
+        ipc->bp[cycle].status = 0;
+        ipc->bp[cycle].timeSec = transmit_time.sec;
+        ipc->bp[cycle].timeNSec = transmit_time.nanosec;
+        ipc->bp[cycle].cycle = cycle;
+
+        ipc->cycle = cycle;
+
+        cycle = (cycle + 1) % 16;
+        transmit_time = transmit_time + time_step;
+
     }
     return 0;
 }
