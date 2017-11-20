5d9eab6f8c4ca294e629f8e73aef13f7387f6849
Jonathan Hanks <jonathan.hanks@ligo.org>
2017-11-20 19:21:04 +0000

    r4553/trunk. Updates to the symmetricom driver
    The symmetricom-4.4 driver now builds cleanly on kernel 3.0.8-4.9.
    
    A new set of interfaces has been added to a gps_offset to be set
    from user space. The goal is that a new year/leap second should
    not require a new code release, simple an update to the offset and
    a restart of models.
    
    Adds a /proc/gps_offset entry and a internal kernel interface
    'long ligo_get_gps_offset(void)' which is consumed by the spectracom driver
    in the IOPs, allowing the IOP to receive the offset as configured in the
    system.
    
    
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/branches/branch-3.4@4556 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/drv/symmetricom-4.4/symmetricom.c
+++ src/drv/symmetricom-4.4/symmetricom.c
@@ -31,11 +31,19 @@
 
 CDS_HARDWARE cdsPciModules;
 
+#define IN_LIGO_GPS_KERNEL_DRIVER 1
+
 #include "../../include/drv/spectracomGPS.c"
 #include "../../include/drv/symmetricomGps.c"
 
-// /proc/gps entry
+#define CDS_LOCAL_GPS_BUFFER_SIZE 30
+
+/* /proc/gps entry */
 struct proc_dir_entry *proc_gps_entry;
+/* /proc/gps_offset entry */
+struct proc_dir_entry *proc_gps_offset_entry;
+
+atomic64_t gps_offset = ATOMIC_INIT(0);
 
 /* character device structures */
 static dev_t symmetricom_dev;
@@ -79,28 +87,31 @@ static int symmetricom_release(struct inode *inode, struct file *filp)
 
 // Read current GPS time from the card
 int get_cur_time(unsigned long *req) {
+  long offset = 0;
   unsigned int timeSec = 0;
   unsigned int timeUsec = 0;
   int sync = 0;
+
+  offset = atomic64_read(&gps_offset);
+
   if (card_present && card_type == 1) {
 	sync = getGpsTimeTsync(&timeSec, &timeUsec);
 	req[0] = timeSec; req[1] = timeUsec; req[2] = 0;
-	return sync;
   } else if (card_present && card_type == 0) {
   	lockGpsTime();
 	sync = getGpsTime(&timeSec, &timeUsec);
 	req[0] = timeSec; req[1] = timeUsec; req[2] = 0;
-	return sync;
   } else {
 	// Get current kernel time (in GPS)
         struct timespec t;
         extern struct timespec current_kernel_time(void);
         t = current_kernel_time();
-        t.tv_sec += - 315964819 + 37;
+        //t.tv_sec += - 315964819 + 37;
 	  req[0] = t.tv_sec; req[1] = t.tv_nsec/1000; req[2] = t.tv_nsec%1000;
-	return 1;
+	  sync = 1;
   }
-  // Never reached
+  req[0] += offset;
+  return sync;
 }
 
 #ifdef HAVE_UNLOCKED_IOCTL
@@ -131,24 +142,6 @@ static int symmetricom_ioctl(struct inode *inode, struct file *filp, unsigned in
         return -EINVAL;
 }
 
-/*
-static void *SINGLE_seq_start(struct seq_file *s, loff_t *pos)
-{
-	if (*pos > 0)
-		return NULL;
-	return (void *)(s);
-}
-
-static void *SINGLE_seq_next(struct seq_file *s, void *v, loff_t *pos)
-{
-	return NULL;
-}
-
-static void SINGLE_seq_stop(struct seq_file *s, void *v)
-{
-}
-*/
-
 /* This is called to translate an item in the sequence to output.
  * In the /proc/gps, there is only one item, and it is the current
  * gps time.
@@ -163,16 +156,8 @@ static int gps_seq_show(struct seq_file *s, void *v)
 	return 0;
 }
 
-/*static struct seq_operations gps_seq_ops = {
-	.start = SINGLE_seq_start,
-	.next = SINGLE_seq_next,
-	.stop = SINGLE_seq_stop,
-	.show = gps_seq_show
-};*/
-
 static int gps_open(struct inode *inode, struct file *file)
 {
-//	return seq_open(file, &gps_seq_ops);
 	return single_open(file, gps_seq_show, NULL);
 }
 
@@ -185,11 +170,107 @@ static struct file_operations gps_file_ops = {
 	.release = single_release
 };
 
+
+static int gps_offset_seq_show(struct seq_file *s, void *v)
+{
+	seq_printf(s, "%ld\n", atomic64_read(&gps_offset));
+	return 0;
+}
+
+static ssize_t gps_offset_write(struct file *f, const char __user *buffer, size_t count,  loff_t *data)
+{
+	int i = 0;
+	long new_offset = 0;
+	char localbuf[CDS_LOCAL_GPS_BUFFER_SIZE+1];
+    size_t full_count = count;
+    size_t remaining = 0;
+    int conv_ret = 0;
+
+    memset(localbuf, 0, CDS_LOCAL_GPS_BUFFER_SIZE+1);
+    // The symmetricom driver doesn't need a tweak on the timing, so don't allow it
+    if (card_present && card_type == 0) {
+        printk("gps_offset_write called when a symmetricom card is present, ignoring the value");
+        return full_count;
+    }
+    // just ignore input when it is too big
+	if (count >= CDS_LOCAL_GPS_BUFFER_SIZE) {
+        printk("gps_offset_write called with too much input, ignoring the value");
+        return -EFAULT;
+    }
+	remaining = copy_from_user(localbuf, buffer, count);
+	if (remaining != 0) {
+        printk("gps_offset_write was unable to move input to userspace");
+        return -EFAULT;
+    }
+    localbuf[CDS_LOCAL_GPS_BUFFER_SIZE] = 0;
+	for (i = 0; localbuf[i] != 0 && i < count; ++i)
+	{
+        if (!((localbuf[i] >= '0' && localbuf[i] <= '9') || (i == 0 && localbuf[i] == '-')))
+        {
+            localbuf[i] = 0;
+            break;
+        }
+	}
+    localbuf[i] = 0;
+    if ((conv_ret = kstrtol(localbuf, 10, &new_offset)) != 0) {
+        printk("gps_offset_write error converting offset into an integer - %s", (conv_ret == -ERANGE ? "range" : "overflow"));
+        return -EFAULT;
+    }
+    atomic64_set(&gps_offset, new_offset);
+    printk("gps_offset_write success - new value = %ld", new_offset);
+    /* tell it we consumed everything */
+	return full_count;
+}
+
+
+static int gps_offset_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, gps_offset_seq_show, NULL);
+}
+
+static struct file_operations gps_offset_file_ops = {
+	.owner = THIS_MODULE,
+	.open = gps_offset_open,
+	.read = seq_read,
+	.write = gps_offset_write,
+	.llseek = seq_lseek,
+	.release = single_release
+};
+
+static void config_proc_entry(struct proc_dir_entry *proc_entry, int uid)
+{
+	/* At kernel 3.10+ these helper functions become available
+	* and the proc_dir_entry becomes an opaque structure
+	*/
+#if	LINUX_VERSION_CODE >= KERNEL_VERSION(3,10,0)
+	proc_set_size(proc_entry , 128);
+	proc_set_user(proc_entry, KUIDT_INIT(uid), KGIDT_INIT(0));
+#else
+	proc_entry->size = 128;
+	proc_entry->uid = uid;
+	proc_entry->gid = 0;
+#endif
+}
+
+/**
+ * ligo_get_gps_driver_offset
+ * @note Exported interface to return the current gps_offset value within the kernel.
+ * @return a long containing the current offset that this driver needs to compute the correct gps time.  This function
+ * does not give any information regarding the epoch that is used, it is specific to the hardware in the machine.
+ * It is exported to help internal LIGO drivers.
+ */
+long ligo_get_gps_driver_offset(void)
+{
+    return atomic64_read(&gps_offset);
+}
+
 /* module initialization - called at module load time */
 static int __init symmetricom_init(void)
 {
         int ret = 0;
-	struct pci_dev *symdev = NULL;
+    struct pci_dev *symdev = NULL;
+    proc_gps_entry = NULL;
+    proc_gps_offset_entry = NULL;
 
         /* get the major number of the character device */
         if ((ret = alloc_chrdev_region(&symmetricom_dev, 0, 1, "symmetricom")) < 0) {
@@ -210,9 +291,14 @@ static int __init symmetricom_init(void)
                 printk(KERN_ALERT "Error: Could not initialize /proc/gps\n");
                 goto out_unalloc_region;
         }
+	config_proc_entry(proc_gps_entry, PROC_UID);
 
-	proc_set_size(proc_gps_entry , 128);
-	proc_set_user(proc_gps_entry, KUIDT_INIT(PROC_UID), KGIDT_INIT(0));
+    proc_gps_offset_entry = proc_create("gps_offset", PROC_MODE | S_IFREG | S_IRUGO, NULL, &gps_offset_file_ops);
+    if (proc_gps_entry == NULL) {
+        printk(KERN_ALERT "Error: Could not initialize /proc/gps_offset\n");
+        goto out_remove_proc_entry;
+    }
+    config_proc_entry(proc_gps_offset_entry, 0);
 
 	/* find the Symmetricom device */
 	symdev = pci_get_device (SYMCOM_VID, SYMCOM_BC635_TID, 0);
@@ -248,9 +334,11 @@ static int __init symmetricom_init(void)
 	} else if (card_type == 0) {
 		symmetricomGpsInit(&cdsPciModules, symdev);
 	}
+
+
         return ret;
-  /*out_remove_proc_entry:
-	remove_proc_entry("gps", NULL);*/
+  out_remove_proc_entry:
+	remove_proc_entry("gps", NULL);
   out_unalloc_region:
         unregister_chrdev_region(symmetricom_dev, 1);
   out:
@@ -264,11 +352,13 @@ static void __exit symmetricom_exit(void)
         cdev_del(&symmetricom_cdev);
         unregister_chrdev_region(symmetricom_dev, 1);
 	remove_proc_entry("gps", NULL);
+	remove_proc_entry("gps_offset", NULL);
 }
 
+EXPORT_SYMBOL(ligo_get_gps_driver_offset);
 module_init(symmetricom_init);
 module_exit(symmetricom_exit);
-MODULE_DESCRIPTION("Symmetricom GPS card driver");
+MODULE_DESCRIPTION("LIGO GPS time driver.  Contains support for using Symmetricom/Spectricom cards and system time.");
 MODULE_AUTHOR("Alex Ivanov aivanov@ligo.caltech.edu");
 MODULE_LICENSE("Dual BSD/GPL");
 
