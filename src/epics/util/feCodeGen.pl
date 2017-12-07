b2ecd1b31b817d6685aef06c19428c13efb58cb8
Rolf Bork <rolf.bork@ligo.org>
2017-12-07 18:15:30 +0000

    Changes for how compile options are set for Dolphin Gen2 and Long Range PCIe network.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/branches/branch-3.4@4585 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/epics/util/feCodeGen.pl
+++ src/epics/util/feCodeGen.pl
@@ -68,6 +68,16 @@ die "Usage: $PROGRAM_NAME <MDL file> <Output file name> [<DCUID number>] [<site>
 #Setup current working directory and pointer to RCG source directory.
 $currWorkDir = &Cwd::cwd();
 $rcg_src_dir = $ENV{"RCG_SRC_DIR"};
+$lrpciefile = $rcg_src_dir . "/src/include/USE_LR_PCIE";
+$pciegenfile = $rcg_src_dir . "/src/include/USE_DOLPHIN_GEN2";
+
+if (-e "$lrpciefile") {
+	print "PCIE LR exists\n";
+	$rfm_via_pcie = 1;
+} else {
+	print "PCIE LR DOES NOT exist\n";
+	$rfm_via_pcie = 0;
+}
 if (! length $rcg_src_dir) { $rcg_src_dir = "$currWorkDir/../../.."; }
 
 @sources = ();
@@ -122,7 +132,6 @@ $rfmTimeSlave = -1;
 $diagTest = -1;
 $flipSignals = 0;
 $virtualiop = 0;
-$rfm_via_pcie = 0;
 $edcu = 0;
 $casdf = 0;
 $globalsdf = 0;
@@ -2542,18 +2551,16 @@ if ($rfmTimeSlave > -1) {
 if ($flipSignals) {
   print OUTM "EXTRA_CFLAGS += -DFLIP_SIGNALS=1\n";
 }
-if ($pciNet == 1) {
-  print OUTM "#Enable use of PCIe RFM Network Gen 1\n";
-  print OUTM "DISDIR = /opt/srcdis\n";
-  print OUTM "EXTRA_CFLAGS += -DOS_IS_LINUX=1 -D_KERNEL=1 -I\$(DISDIR)/src/IRM/drv/src -I\$(DISDIR)/src/IRM/drv/src/LINUX -I\$(DISDIR)/src/include -I\$(DISDIR)/src/include/dis -DDOLPHIN_TEST=1  -DDIS_BROADCAST=0x80000000\n";
-} elsif($pciNet == 2) {
-  print OUTM "#Enable use of PCIe RFM Network Gen 2\n";
-  print OUTM "DISDIR = /opt/srcdis\n";
-  print OUTM "EXTRA_CFLAGS += -DOS_IS_LINUX=1 -D_KERNEL=1 -I\$(DISDIR)/src/IRM_GX/drv/src -I\$(DISDIR)/src/IRM_GX/drv/src/LINUX -I\$(DISDIR)/src/include -I\$(DISDIR)/src/include/dis -I\$(DISDIR)/src/COMMON/osif/kernel/include -I\$(DISDIR)/src/COMMON/osif/kernel/include/LINUX -DDOLPHIN_TEST=1  -DDIS_BROADCAST=0x80000000\n";
-} else {
-  print OUTM "#Uncomment to use PCIe RFM Network\n";
-  print OUTM "#DISDIR = /home/controls/DIS\n";
-  print OUTM "#EXTRA_CFLAGS += -DOS_IS_LINUX=1 -D_KERNEL=1 -I\$(DISDIR)/src/IRM/drv/src -I\$(DISDIR)/src/IRM/drv/src/LINUX -I\$(DISDIR)/src/include -I\$(DISDIR)/src/include/dis -DDOLPHIN_TEST=1  -DDIS_BROADCAST=0x80000000\n";
+if ($pciNet) {
+	if (-e "$pciegenfile") {
+	  print OUTM "#Enable use of PCIe RFM Network Gen 2\n";
+	  print OUTM "DISDIR = /opt/srcdis\n";
+	  print OUTM "EXTRA_CFLAGS += -DOS_IS_LINUX=1 -D_KERNEL=1 -I\$(DISDIR)/src/IRM_GX/drv/src -I\$(DISDIR)/src/IRM_GX/drv/src/LINUX -I\$(DISDIR)/src/include -I\$(DISDIR)/src/include/dis -I\$(DISDIR)/src/COMMON/osif/kernel/include -I\$(DISDIR)/src/COMMON/osif/kernel/include/LINUX -DDOLPHIN_TEST=1  -DDIS_BROADCAST=0x80000000\n";
+	} else {
+	  print OUTM "#Enable use of PCIe RFM Network Gen 1\n";
+	  print OUTM "DISDIR = /opt/srcdis\n";
+	  print OUTM "EXTRA_CFLAGS += -DOS_IS_LINUX=1 -D_KERNEL=1 -I\$(DISDIR)/src/IRM/drv/src -I\$(DISDIR)/src/IRM/drv/src/LINUX -I\$(DISDIR)/src/include -I\$(DISDIR)/src/include/dis -DDOLPHIN_TEST=1  -DDIS_BROADCAST=0x80000000\n";
+	} 
 }
 if($virtualiop > 1) {
   print OUTM "EXTRA_CFLAGS += -DDOLPHIN_SWITCH=1\n";
