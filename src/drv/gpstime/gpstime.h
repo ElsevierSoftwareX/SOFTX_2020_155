43188ac924b0da06765601bcd2f7e4065c87a120
jonathan.hanks@LIGO.ORG <jonathan.hanks@LIGO.ORG@6dcd42c9-f523-4c6d-aada-af552506706e>
2018-01-03 01:14:50 +0000

    Enable more automatic offset calculation for the gps_time driver.
    Export timing card status, gps offsets, gps time, ... via sysfs.  This
    includes exporting the type of gps_offset needed via
    /sys/kernel/gpstime/offset_type so that a userspace program can
    determine how to calculate the proper gps offset.
    
    Updated the calc_gps_offset.py program to have a --status and --auto/-a
    flags to show card status and determine the type of gps_offset to
    produce.
    
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/branches/branch-3.4@4607 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/drv/gpstime/gpstime.h
+++ src/drv/gpstime/gpstime.h
@@ -5,3 +5,9 @@
 // Get current GPS time
 #define IOCTL_SYMMETRICOM_TIME 1
 
+// Define the type of manual synchronization that
+// must be done to the system to get correct time
+#define STATUS_SYMMETRICOM_NO_SYNC 0
+#define STATUS_SYMMETRICOM_YEAR_SYNC 1
+#define STATUS_SYMMETRICOM_LOCALTIME_SYNC 2
+
