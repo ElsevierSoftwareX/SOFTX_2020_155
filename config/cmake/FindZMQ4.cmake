619840776283a7a3f1e75c8099da63f31d7f6a15
Jonathan Hanks <jonathan.hanks@ligo.org>
2018-01-04 19:32:37 +0000

    Forward port r4601.  Vendor a copy of the zmq c++ bindings
    Also adjusts the build system to use the vendored copy of the zmq
    bindings if the zmq.hpp file cannot be found.
    
    
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4616 6dcd42c9-f523-4c6d-aada-af552506706e

--- config/cmake/FindZMQ4.cmake
+++ config/cmake/FindZMQ4.cmake
@@ -21,7 +21,14 @@ if (LibZMQ_FOUND)
     add_library(_zmq4_intl INTERFACE)
     target_include_directories(_zmq4_intl INTERFACE ${LibZMQ_INCLUDE_DIRS})
     target_link_libraries(_zmq4_intl INTERFACE _zmq4)
-    
+
+    find_file(_zmq4_hpp zmq.hpp
+            PATHS ${libZMQ_INCLUDE_DIRS})
+    if (NOT ${_zmq4_hpp})
+        target_include_directories(_zmq4_intl INTERFACE ${PROJECT_SOURCE_DIR}/src/include/thirdparty/zmq)
+        message("Adding ${PROJECT_SOURCE_DIR}/src/include/thirdparty/zmq to the include list for zmq4")
+    endif(NOT ${_zmq4_hpp})
+
     # Give the interface library a nice name for exporting
     add_library(zmq4::zmq ALIAS _zmq4_intl)
 endif(LibZMQ_FOUND)
