c3021b291ad2e4036ac08ee59f1adf62e4fae3bf
Rolf Bork <rolf.bork@ligo.org>
2018-03-16 19:27:23 +0000

    Updated to latest long range PCIe epics code from branch-3.4.
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4699 6dcd42c9-f523-4c6d-aada-af552506706e

--- src/pcie_switch/cdsrfmepics/cdsrfmmonitor.py
+++ src/pcie_switch/cdsrfmepics/cdsrfmmonitor.py
@@ -1,56 +1,155 @@
 #!/usr/bin/env python
 # Code to interface cdsrfmswitch kernel module code and EPICS database
 
-import epics
 import time
 import string
 import sys
 import os
-
-from epics import PV
 import argparse
+import threading
+
+# get CDS EPICS environment
+sys.path.append('/opt/cdscfg')
+import subprocess
+import stdenv as cds
+cds.INIT_ENV()
+
+# now get EPICS python
+from pcaspy import SimpleServer, Driver
+
 
 parser = argparse.ArgumentParser(description='Provides interface between cdsrfmswitch kernel code and cdsrfmswitch EPICS database. This code MUST run only on the cdsrfmswitch computer.')
 parser.add_argument("ifo",
 			help = "Takes IFO prefix (X2,H1,L1,etc.) as argument")
 args = parser.parse_args()
 
-epics_chan = []
-chan_list = (':CDS-RFM_SWITCH_CHCNT1',
-		':CDS-RFM_SWITCH_CHCNT2',
-		':CDS-RFM_SWITCH_CHCNT3',
-		':CDS-RFM_SWITCH_CHCNT4',
-		':CDS-RFM_SWITCH_TIME',
-		':CDS-RFM_SWITCH_ACTIVE01',
-		':CDS-RFM_SWITCH_ACTIVE02',
-		':CDS-RFM_SWITCH_ACTIVE03',
-		':CDS-RFM_SWITCH_ACTIVE11',
-		':CDS-RFM_SWITCH_ACTIVE12',
-		':CDS-RFM_SWITCH_ACTIVE13',
-		':CDS-RFM_SWITCH_ACTIVE21',
-		':CDS-RFM_SWITCH_ACTIVE22',
-		':CDS-RFM_SWITCH_ACTIVE23',
-		':CDS-RFM_SWITCH_ACTIVE31',
-		':CDS-RFM_SWITCH_ACTIVE32',
-		':CDS-RFM_SWITCH_ACTIVE33',
-		':CDS-RFM_SWITCH_STATUS')
+prefix = args.ifo + ':CDS-RFM_LRS_'
 
-for item in chan_list:
-	chan_name = args.ifo + item;
-	epics_chan.append(PV(chan_name))
+epics_chan = []
+value = 0
+chan_list = ('EX2CS_CHCNT',
+		'CS2EX_CHCNT',
+		'CS2EY_CHCNT',
+		'EY2CS_CHCNT',
+		'TIME',
+		'EX2CS_ACTIVE1',
+		'EX2CS_ACTIVE2',
+		'ACTIVE03',
+		'CS2EX_ACTIVE1',
+		'CS2EX_ACTIVE2',
+		'ACTIVE13',
+		'CS2EY_ACTIVE1',
+		'CS2EY_ACTIVE2',
+		'ACTIVE23',
+		'EY2CS_ACTIVE1',
+		'EY2CS_ACTIVE2',
+		'ACTIVE33',
+		'STATUS')
+pvdb = {
+    	chan_list[0] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[1] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[2] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[3] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[4] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[5] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[6] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[7] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[8] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[9] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[10] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[11] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[12] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[13] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[14] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[15] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[16] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+    	chan_list[17] : {
+		'type' : 'int', 
+		'scan' : 1,
+	},
+}
+
+class myDriver(Driver):
+	def __init__(self):
+		super(myDriver, self).__init__()
+
+	def read(self, reason):
+		value = 0
+		if reason == chan_list[4]:
+			try:
+				# Read /proc file produced by kmod code
+				with open("/proc/cdsrfm","r") as datafile:
+					first_line = datafile.readline()
+				word = first_line.split()
+
+				# Relay info to EPICS
+				for ii,data in enumerate(word):
+					self.setParam(chan_list[ii], int(data))
+				value = self.getParam(reason)
+				datafile.close()
+			except:
+				# If /proc file does not exist, send fault status to EPICS
+				self.setParam(chan_list[ii], 0)
+		else:
+			value = self.getParam(reason)
+		return value
+
+if __name__ == '__main__':
+	server = SimpleServer()
+	server.createPV(prefix, pvdb)
+	driver = myDriver()
+
 
 while True:
-	time.sleep(1)
-	try:
-		# Read /proc file produced by kmod code
-		with open("/proc/cdsrfm","r") as datafile:
-			first_line = datafile.readline()
-		word = first_line.split()
-
-		# Relay info to EPICS
-		for ii,data in enumerate(word):
-			epics_chan[ii].value = data
-	except:
-		# If /proc file does not exist, send fault status to EPICS
-		epics_chan[17].value = 0
-
+	server.process(1)
