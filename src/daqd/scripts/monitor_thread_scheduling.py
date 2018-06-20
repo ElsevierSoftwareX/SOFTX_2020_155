4732218b89b3e2d8a08564f42cc6411c5cb81157
Jonathan Hanks <jonathan.hanks@ligo.org>
2018-06-20 16:28:16 +0000

    Added a script to export thread scheduling information to epics
    Added monitor_thread_scheduling.py, this is a pcaspy script which takes
    two arguments, a PID and a prefix.  It then exports a set of PVs,
    currently one per child thread of the given PID that contains the last
    cpu the thread was scheduled on.  It refreshes the data at a 4hz rate.
    
    git-svn-id: https://redoubt.ligo-wa.caltech.edu/svn/advLigoRTS/trunk@4760 6dcd42c9-f523-4c6d-aada-af552506706e

---
+++ src/daqd/scripts/monitor_thread_scheduling.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import glob
+import sys
+import time
+
+from pcaspy import SimpleServer, Driver
+
+
+class RODriver(Driver):
+    def __init__(self):
+        super(RODriver, self).__init__()
+
+    def write(self, reason, value):
+        return False
+
+
+def sanitize_name(name):
+    def filter_func (x):
+        return x if x in safe else '_'
+
+    safe = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-:0123456789'
+    return "".join(map(filter_func, name))
+
+
+def get_child_info(pid):
+    info = {}
+    file_glob = "/proc/{0}/task/*/stat".format(pid)
+    for fname in glob.glob(file_glob):
+        with open(fname, 'rt') as f:
+            data = f.read()
+            parts = data.split()
+            name = sanitize_name(parts[1])
+            processor = int(parts[38])
+            info[name] = processor
+    return info
+
+
+def create_db(info):
+    db = {}
+    for thread_name in info:
+        entry = {
+            'value': info[thread_name],
+        }
+        db[thread_name] = entry
+        print(thread_name)
+    print(db)
+    return db
+
+
+def main():
+    pid = int(sys.argv[1])
+    prefix = sys.argv[2]
+    print("pid = {0}".format(pid))
+    info = get_child_info(pid)
+    db = create_db(info)
+
+    server = SimpleServer()
+    server.createPV(prefix, db)
+    driver = RODriver()
+    while True:
+        next = time.time() + 0.25
+        server.process(0.25)
+
+        while time.time() < next:
+            time.sleep(0.01)
+
+        info = get_child_info(pid)
+        for thread_name in info:
+            if thread_name in db:
+                driver.setParam(thread_name, info[thread_name])
+        driver.updatePVs()
+
+main()
