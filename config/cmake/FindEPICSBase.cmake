186eb4a54795013bb74dc753ca8828ae005bd7e3
Jonathan Hanks <jonathan.hanks@ligo.org>
2019-09-12 09:36:08 -0700

    WIP on doing a standalone edcu application.
    Introducing a new edu application.
    
    This is a regular application, based on the edcu iocsh application, but
    without being an epics iocsh.  This is oriented towards a quick deployment
    of an epics->frame archiver w/o setting up an edc model.

--- config/cmake/FindEPICSBase.cmake
+++ config/cmake/FindEPICSBase.cmake
@@ -7,16 +7,19 @@ if (EPICS_BASE_FOUND)
 
         set (_epics_calibs_libs_intl "")
         set (_epics_caslibs_libs_intl "")
+        set (_epics_seqlibs_libs_intl "")
 
     else (${EPICS_BASE_VERSION} VERSION_LESS "3.15")
 
         set (_epics_calibs_libs_intl "ca;Com")
         set (_epics_caslibs_libs_intl "cas")
+        set (_epics_seqlibs_libs_intl "seq;pv;dbRecStd;dbCore")
 
     endif (${EPICS_BASE_VERSION} VERSION_LESS "3.15")
 
     set (EPICS_BASE_CAS_LIBS "")
     set (EPICS_BASE_CA_LIBS "")
+    set (EPICS_BASE_SEQ_LIBS "")
 
     foreach (_ca_lib IN LISTS _epics_calibs_libs_intl)
         message("Searching for ${_ca_lib} from ${_epics_calibs_libs_intl}")
@@ -31,6 +34,13 @@ if (EPICS_BASE_FOUND)
         list(APPEND EPICS_BASE_CAS_LIBS "${_concrete_cas_lib_${_cas_lib}}")
     endforeach(_cas_lib)
     message("CAS LIBS = ${EPICS_BASE_CAS_LIBS}")
+
+    foreach (_seq_lib IN LISTS _epics_seqlibs_libs_intl)
+        message("Searching for ${_seq_lib} from ${_epics_seqlibs_libs_intl}")
+        find_library("_concrete_seq_lib_${_seq_lib}" ${_seq_lib} HINTS ${EPICS_BASE_LIBRARY_DIRS})
+        list(APPEND EPICS_BASE_SEQ_LIBS "${_concrete_seq_lib_${_seq_lib}}")
+    endforeach(_seq_lib)
+    message("SEQ LIBS = ${EPICS_BASE_SEQ_LIBS}")
 
     add_library(_epics_base_ca_intl INTERFACE)
     target_include_directories(_epics_base_ca_intl INTERFACE
@@ -48,12 +58,21 @@ if (EPICS_BASE_FOUND)
     target_link_libraries(_epics_base_cas_intl INTERFACE
             ${EPICS_BASE_CAS_LIBS})
 
+    add_library(_epics_base_seq_intl INTERFACE)
+    target_include_directories(_epics_base_seq_intl INTERFACE
+            ${EPICS_BASE_INCLUDE_DIRS})
+    target_compile_options(_epics_base_seq_intl INTERFACE
+            ${EPICS_BASE_CFLAGS_OTHER})
+    target_link_libraries(_epics_base_seq_intl INTERFACE
+            ${EPICS_BASE_SEQ_LIBS})
+
     find_path(_epics_gdd_include gdd.h
             hints ${EPICS_BASE_INCLUDE_DIRS})
     find_library(_epics_gdd_lib gdd HINTS ${EPICS_BASE_LIBRARY_DIRS})
 
     add_library(epics::ca ALIAS _epics_base_ca_intl)
     add_library(epics::cas ALIAS _epics_base_cas_intl)
+    add_library(epics::seq ALIAS _epics_base_seq_intl)
 
     if (_epics_gdd_include AND _epics_gdd_lib)
 
