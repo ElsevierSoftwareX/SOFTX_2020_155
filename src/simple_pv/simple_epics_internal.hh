1b8b1a5487c070479ce29f05fe4fe3deada09ec6
Jonathan Hanks <jonathan.hanks@ligo.org>
2020-01-27 15:12:37 -0600

    Minor changes to the simple_pv code to build on Debian 8.
    Ensure simple_pv is marked as C++11, add a make_unique (which is
    missing in C++11).

--- src/simple_pv/simple_epics_internal.hh
+++ src/simple_pv/simple_epics_internal.hh
@@ -6,12 +6,35 @@
 #define DAQD_TRUNK_SIMPLE_PV_INTERNAL_HH
 
 #include "simple_epics.hh"
+#include <memory>
 
 namespace simple_epics
 {
 
     namespace detail
     {
+
+        // std::make_unique didn't make it into C++11, so
+        // to allow this to work in a pre C++14 world, we
+        // provide a simple replacement.
+        //
+        // A make_unique<> for C++11.  Taken from
+        // "Effective Modern C++ by Scott Meyers (O'Reilly).
+        // Copyright 2015 Scott Meyers, 978-1-491-90399-5"
+        //
+        // Permission given in the book to reuse code segments.
+        //
+        // @tparam T The type of the object to be managed by the unique_ptr
+        // @tparam Ts The type of the arguments to T's constructor
+        // @param params The arguments to forward to the constructor
+        // @return a std::unique_ptr<T>
+        template < typename T, typename... Ts >
+        std::unique_ptr< T >
+        make_unique_ptr( Ts&&... params )
+        {
+            return std::unique_ptr< T >( new T( std::forward< Ts >( params )... ) );
+        }
+
         class setup_int_pv_table;
 
         /*!
