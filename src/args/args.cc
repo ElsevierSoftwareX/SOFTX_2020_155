3bf03feeccded4bfa6f1e5f8ef0b29a3f38b86fe
Jonathan Hanks <jonathan.hanks@ligo.org>
2020-01-31 18:57:52 -0600

    Changes to allow the args library to build on Debian 8

--- src/args/args.cc
+++ src/args/args.cc
@@ -15,6 +15,28 @@
 
 namespace
 {
+
+    // std::make_unique didn't make it into C++11, so
+    // to allow this to work in a pre C++14 world, we
+    // provide a simple replacement.
+    //
+    // A make_unique<> for C++11.  Taken from
+    // "Effective Modern C++ by Scott Meyers (O'Reilly).
+    // Copyright 2015 Scott Meyers, 978-1-491-90399-5"
+    //
+    // Permission given in the book to reuse code segments.
+    //
+    // @tparam T The type of the object to be managed by the unique_ptr
+    // @tparam Ts The type of the arguments to T's constructor
+    // @param params The arguments to forward to the constructor
+    // @return a std::unique_ptr<T>
+    template < typename T, typename... Ts >
+    std::unique_ptr< T >
+    make_unique_ptr( Ts&&... params )
+    {
+        return std::unique_ptr< T >( new T( std::forward< Ts >( params )... ) );
+    }
+
     struct arg_parser
     {
         explicit arg_parser( std::string program_description )
@@ -94,7 +116,7 @@ const char* ARGS_NO_LONG = nullptr;
 args_handle
 args_create_parser( const char* program_description ) try
 {
-    auto parser = std::make_unique< arg_parser >( program_description );
+    auto parser = make_unique_ptr< arg_parser >( program_description );
     args_add_flag( to_handle( parser.get( ) ),
                    'h',
                    "help",
