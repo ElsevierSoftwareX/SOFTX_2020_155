672fa44998e1b5508f37646e7b7bd56cb5eb51a6
Jameson Graef Rollins <jameson.rollins@ligo.org>
2020-04-28 10:55:14 -0700

    remove the old athena directory
    closes #99

--- athena/startup.cmd
+++
@@ -1,6 +0,0 @@
-#dbLoadDatabase "../../dbd/athena.dbd"
-#registerRecordDeviceDriver(pdbbase)
-dbLoadRecords "a.db"
-dbLoadTemplate "/target/pid_control.template"
-#dbLoadRecords "/target/pid_control.db"
-iocInit
